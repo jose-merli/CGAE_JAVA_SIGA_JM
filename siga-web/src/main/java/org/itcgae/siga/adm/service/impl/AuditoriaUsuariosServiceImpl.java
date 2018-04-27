9031522d8c093ef1d1a40a9f0bb0185715f60890
Manuel Jimenez <mjimenezmolina@deloitte.es>
2018-04-27 15:05:28 +0200

    [mjimenezmolina] Implementación update para back de auditoria de usuarios.

--- siga-web/src/main/java/org/itcgae/siga/adm/service/impl/AuditoriaUsuariosServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/adm/service/impl/AuditoriaUsuariosServiceImpl.java
@@ -2,6 +2,7 @@ package org.itcgae.siga.adm.service.impl;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,7 @@ import org.itcgae.siga.DTOs.gen.ComboItem;
 import org.itcgae.siga.adm.service.IAuditoriaUsuariosService;
 import org.itcgae.siga.db.entities.AdmUsuarios;
 import org.itcgae.siga.db.entities.AdmUsuariosExample;
+import org.itcgae.siga.db.entities.CenHistorico;
 import org.itcgae.siga.db.services.adm.mappers.AdmUsuariosExtendsMapper;
 import org.itcgae.siga.db.services.adm.mappers.CenHistoricoExtendsMapper;
 import org.itcgae.siga.db.services.adm.mappers.CenTipocambioExtendsMapper;
@@ -94,7 +96,34 @@ public class AuditoriaUsuariosServiceImpl implements IAuditoriaUsuariosService{
 	@Override
 	public UpdateResponseDTO auditUsersUpdate(HistoricoUsuarioUpdateDTO historicoUsuarioUpdateDTO,
 			HttpServletRequest request) {
-		// TODO Auto-generated method stub
+		int response = 0;
+		UpdateResponseDTO updateResponseDTO = new UpdateResponseDTO();
+		
+		// Conseguimos idLenguaje e idInstitucion del usuario logeado
+		String nifInstitucion = UserAuthenticationToken.getUserFromJWTToken(request.getHeader("Authorization"));
+		String dni = nifInstitucion.substring(0, 9);
+		String idInstitucion = nifInstitucion.substring(nifInstitucion.length() - 4, nifInstitucion.length());
+		AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
+		exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
+		List<AdmUsuarios> usuarios = admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
+		AdmUsuarios usuario = usuarios.get(0);
+		
+		//clave primaria(idPersona, idInstitucion, idHistorico)
+		CenHistorico record =  new CenHistorico();
+		record.setMotivo(historicoUsuarioUpdateDTO.getMotivo());
+		record.setUsumodificacion(usuario.getIdusuario());
+		record.setFechamodificacion(new Date());
+		record.setIdpersona(Long.valueOf(historicoUsuarioUpdateDTO.getIdPersona()));
+		record.setIdinstitucion(Short.valueOf(idInstitucion));
+		record.setIdhistorico(Short.valueOf(historicoUsuarioUpdateDTO.getIdHistorico()));
+		
+		cenHistoricoExtendsMapper.updateByPrimaryKeySelective(record);
+
+		if(response == 1)
+			updateResponseDTO.setStatus("OK");
+		else
+			updateResponseDTO.setStatus("ERROR");
+		
 		return null;
 	}
 
