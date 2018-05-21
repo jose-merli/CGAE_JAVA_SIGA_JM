86e153eea1e0a43032b8ab3b4db382f6e92fe734
Manuel Jimenez <mjimenezmolina@deloitte.es>
2018-05-21 15:46:15 +0200

    [mjimenezmolina] Codigo comentado para compilacion correcta del servicio busqueda persona juridica

--- siga-web/src/main/java/org/itcgae/siga/cen/controllers/BusquedaPerJuridicaController.java
+++ siga-web/src/main/java/org/itcgae/siga/cen/controllers/BusquedaPerJuridicaController.java
@@ -2,14 +2,18 @@ package org.itcgae.siga.cen.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.itcgae.siga.DTOs.adm.ContadorDTO;
+import org.itcgae.siga.DTOs.adm.ContadorRequestDTO;
 import org.itcgae.siga.DTOs.gen.ComboDTO;
 import org.itcgae.siga.cen.services.IBusquedaPerJuridicaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,18 +23,25 @@ public class BusquedaPerJuridicaController {
 	@Autowired
 	private IBusquedaPerJuridicaService busquedaPerJuridicaService;
 	
-	@RequestMapping(value = "busquedaPerJuridica/tipoSociedad",  method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/busquedaPerJuridica/tipoSociedad",  method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ComboDTO> getSocietyTypes( HttpServletRequest request) {
 		ComboDTO response = busquedaPerJuridicaService.getSocietyTypes(request);
 		return new ResponseEntity<ComboDTO>(response, HttpStatus.OK);
 	}
 	
 	
-	@RequestMapping(value = "busquedaPerJuridica/etiqueta",  method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/busquedaPerJuridica/etiqueta",  method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ComboDTO> getLabel( HttpServletRequest request) {
 		ComboDTO response = busquedaPerJuridicaService.getLabel(request);
 		return new ResponseEntity<ComboDTO>(response, HttpStatus.OK);
 	}
+	
+//	@RequestMapping(value = "busquedaPerJuridica/search", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+//	ResponseEntity<BusquedaJuridicaDto> searchLegalPersons(@RequestParam("numPagina") int numPagina, @RequestBody BusquedaJuridicaSearchDto busquedaJuridicaSearchDto, HttpServletRequest request) { 
+//		BusquedaJuridicaDto response = busquedaPerJuridicaService.searchLegalPersons(numPagina, busquedaJuridicaSearchDto, request);
+//		return new ResponseEntity<BusquedaJuridicaDto >(response, HttpStatus.OK);
+//	}
+	
 	
 	
 	
--- siga-web/src/main/java/org/itcgae/siga/cen/services/IBusquedaPerJuridicaService.java
+++ siga-web/src/main/java/org/itcgae/siga/cen/services/IBusquedaPerJuridicaService.java
@@ -3,10 +3,6 @@ package org.itcgae.siga.cen.services;
 import javax.servlet.http.HttpServletRequest;
 
 import org.itcgae.siga.DTOs.gen.ComboDTO;
-import org.itcgae.siga.cen.controllers.BusquedaJuridicaDto;
-import org.itcgae.siga.cen.controllers.BusquedaJuridicaSearchDto;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface IBusquedaPerJuridicaService {
 	
@@ -14,5 +10,5 @@ public interface IBusquedaPerJuridicaService {
 	
 	public ComboDTO getLabel( HttpServletRequest request);
 	
-	public BusquedaJuridicaDto searchLegalPersons(int numPagina, BusquedaJuridicaSearchDto busquedaJuridicaSearchDto, HttpServletRequest request);
+	//public BusquedaJuridicaDto searchLegalPersons(int numPagina, BusquedaJuridicaSearchDto busquedaJuridicaSearchDto, HttpServletRequest request);
 }
