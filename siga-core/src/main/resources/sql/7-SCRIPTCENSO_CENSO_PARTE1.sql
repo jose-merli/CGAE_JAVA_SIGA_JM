0c5aabadd4b97d65f62701ad82e39e3e6dae0da7
Jesús Colchón Corbacho <jcolchon@deloitte.es>
2018-05-30 19:20:03 +0200

    [jcolchon] solucionado error con rueda de configuracion

--- siga-core/src/main/resources/sql/7-SCRIPTCENSO_CENSO_PARTE1.sql
+++ siga-core/src/main/resources/sql/7-SCRIPTCENSO_CENSO_PARTE1.sql
@@ -140,4 +140,7 @@ Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFIC
 Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.consultaDatos.literal.fechaBaja','Fecha Baja#EU','0','3',to_date('10/12/05','DD/MM/RR'),'0','19');
 Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.consultaDatos.literal.fechaBaja','Data Baixa','0','2',to_date('10/12/05','DD/MM/RR'),'0','19');
 
+Update gen_menu Set orden = orden + 13000 Where idmenu In ('90','91') Or idparent In ('90','91','I00','019','125','126','127','92','93','97','99A','99B','310','311','300','132','135','95','96');
+
+
 COMMIT;
