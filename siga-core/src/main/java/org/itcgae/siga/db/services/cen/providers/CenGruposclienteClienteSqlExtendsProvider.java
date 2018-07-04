21bb4ac09490b9bc3b0c3381c7f0fe241f6f3c11
Manuel Jimenez <mjimenezmolina@deloitte.es>
2018-07-04 11:36:07 +0200

    [mjimenezmolina] Cambios para guardar correctamente etiquetas con una sociedad ya existente

--- siga-core/src/main/java/org/itcgae/siga/db/services/cen/providers/CenGruposclienteClienteSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/cen/providers/CenGruposclienteClienteSqlExtendsProvider.java
@@ -1,6 +1,7 @@
 package org.itcgae.siga.db.services.cen.providers;
 
 import org.apache.ibatis.jdbc.SQL;
+import org.itcgae.siga.DTOs.cen.EtiquetaUpdateDTO;
 import org.itcgae.siga.db.mappers.CenGruposclienteClienteSqlProvider;
 
 public class CenGruposclienteClienteSqlExtendsProvider extends CenGruposclienteClienteSqlProvider{
@@ -38,6 +39,28 @@ public class CenGruposclienteClienteSqlExtendsProvider extends CenGruposclienteC
 		sql.WHERE("cli.FECHA_BAJA is null");
 		sql.WHERE("idinstitucion = '"+idInstitucion+"'");
 		
+		return sql.toString();
+	}
+	
+	public String insertSelectiveForUpdateLegalPerson(EtiquetaUpdateDTO etiquetaUpdateDTO, String idInstitucion, String grupo, String idUsuario) {
+		SQL sql = new SQL();
+		
+		sql.INSERT_INTO("CEN_GRUPOSCLIENTE_CLIENTE");
+		sql.VALUES("IDPERSONA", "'" + etiquetaUpdateDTO.getIdPersona() + "'");
+		sql.VALUES("IDINSTITUCION", idInstitucion);
+		
+		if(!grupo.equals("")) {
+			sql.VALUES("IDGRUPO", "'" +grupo + "'");
+		}
+		else {
+			sql.VALUES("IDGRUPO", "(SELECT MAX(IDGRUPO) FROM CEN_GRUPOSCLIENTE)");
+		}
+		
+		sql.VALUES("FECHAMODIFICACION", "SYSDATE");
+		sql.VALUES("USUMODIFICACION", "'" +idUsuario + "'");
+		sql.VALUES("IDINSTITUCION_GRUPO", "'" +idInstitucion+ "'");
+		sql.VALUES("FECHA_BAJA", "null");
+		sql.VALUES("FECHA_INICIO", "SYSDATE");
 		return sql.toString();
 	}
 	
