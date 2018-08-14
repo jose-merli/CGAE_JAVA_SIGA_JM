7db7eca86b991fb38bbc3f982283e4ea194e6b73
dsutil <dtuser@deloitte.es>
2018-08-14 13:56:12 +0200

    Estructura de BD según SVN de CGAE

---
+++ siga-core/src/main/resources/bd/PRE_001_001/2018-08-10-01- 13-OPCIONES_MARCO_CONVIVENCIA_01_Incidencia_SIGARNV-194.sql
@@ -0,0 +1,6 @@
+--PRODUCTOS Y SERVICIOS
+update gen_menu set path = 'opcionMenu' where idMenu = '052';
+update gen_menu set path = 'categoriasProducto' where idMenu = '400';
+update gen_menu set path = 'categoriasServicios' where idMenu = '401';
+
+commit;
