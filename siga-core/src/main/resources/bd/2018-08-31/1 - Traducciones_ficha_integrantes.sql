5570101d2104692b6efc673698d4f4c9b9fc5e53
Manuel Jimenez <mjimenezmolina@deloitte.es>
2018-08-29 15:01:48 +0200

    [mjimenezmolina] Scripts para incidencia SIGARNV- 97. Se ponen perfilpordefecto = S.  Añadidas traducciones para tarjeta de integrantes

---
+++ siga-core/src/main/resources/bd/2018-08-31/1 - Traducciones_ficha_integrantes.sql
@@ -0,0 +1,7 @@
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.fichaIntegrantes.literal.estado','Estado','0','1',to_date('10/03/04','DD/MM/RR'),'0','15');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.fichaIntegrantes.literal.estado','Estat','0','2',to_date('10/03/04','DD/MM/RR'),'0','15');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.fichaIntegrantes.literal.estado','Estado#EU','0','3',to_date('10/03/04','DD/MM/RR'),'0','15');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('censo.fichaIntegrantes.literal.estado','Estado#GL','0','4',to_date('10/03/04','DD/MM/RR'),'0','15');
+
+
+commit;
