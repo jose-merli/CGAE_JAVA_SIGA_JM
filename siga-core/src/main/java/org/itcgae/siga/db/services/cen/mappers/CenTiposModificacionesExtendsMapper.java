494195318369b95dba5866b22e55b4da6d0cacd9
Ainara <acorta@deloitte.es>
2018-10-24 11:20:19 +0200

    [acorta] Pantalla Modificación de Datos en proceso.

--- siga-core/src/main/java/org/itcgae/siga/db/services/cen/mappers/CenTiposModificacionesExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/cen/mappers/CenTiposModificacionesExtendsMapper.java
@@ -6,7 +6,12 @@ import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.SelectProvider;
 import org.apache.ibatis.type.JdbcType;
+import org.itcgae.siga.DTOs.cen.SolIncorporacionItem;
+import org.itcgae.siga.DTOs.cen.SolModificacionItem;
+import org.itcgae.siga.DTOs.cen.SolicitudIncorporacionSearchDTO;
+import org.itcgae.siga.DTOs.cen.SolicitudModificacionSearchDTO;
 import org.itcgae.siga.DTOs.gen.ComboItem;
+import org.itcgae.siga.db.services.cen.providers.CenSolicitudincorporacionSqlExtendsProvider;
 import org.itcgae.siga.db.services.cen.providers.CenTiposModificacionesSqlExtendsProvider;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -22,4 +27,13 @@ public interface CenTiposModificacionesExtendsMapper {
 	})
 	List<ComboItem> getTipoModificacion(String idLenguage);
 
+	@SelectProvider(type = CenTiposModificacionesSqlExtendsProvider.class, method = "searchModificationRequest")
+	@Results({ 	@Result(column = "ESTADO", property = "estado", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "IDSOLICITUD", property = "idSolicitud", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOMODIFICACION", property = "tipoModificacion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "NCOLEGIADO", property = "numColegiado", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "FECHAALTA", property = "fechaAlta", jdbcType = JdbcType.DATE)
+			})
+	List<SolModificacionItem> searchModificationRequest(SolicitudModificacionSearchDTO solicitudModificacionSearchDTO, String idLenguage);
 }
--- siga-web/src/main/java/org/itcgae/siga/cen/services/impl/SolicitudModificacionServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/cen/services/impl/SolicitudModificacionServiceImpl.java
@@ -5,13 +5,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.itcgae.siga.DTOs.cen.SolModificacionDTO;
+import org.itcgae.siga.DTOs.cen.SolModificacionItem;
+import org.itcgae.siga.DTOs.cen.SolicitudModificacionSearchDTO;
 import org.itcgae.siga.DTOs.gen.ComboDTO;
 import org.itcgae.siga.DTOs.gen.ComboItem;
 import org.itcgae.siga.cen.services.ISolicitudModificacionService;
 import org.itcgae.siga.db.entities.AdmUsuarios;
 import org.itcgae.siga.db.entities.AdmUsuariosExample;
 import org.itcgae.siga.db.services.adm.mappers.AdmUsuariosExtendsMapper;
-import org.itcgae.siga.db.services.cen.mappers.CenEstadoSolicitudExtendsMapper;
 import org.itcgae.siga.db.services.cen.mappers.CenEstadoSolicitudModifExtendsMapper;
 import org.itcgae.siga.db.services.cen.mappers.CenTiposModificacionesExtendsMapper;
 import org.itcgae.siga.security.UserTokenUtils;
@@ -19,30 +21,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SolicitudModificacionServiceImpl implements ISolicitudModificacionService{
+public class SolicitudModificacionServiceImpl implements ISolicitudModificacionService {
 
 	private Logger LOGGER = Logger.getLogger(SolicitudModificacionServiceImpl.class);
 
 	@Autowired
 	private AdmUsuariosExtendsMapper _admUsuariosExtendsMapper;
-	
+
 	@Autowired
 	private CenTiposModificacionesExtendsMapper cenTiposModificacionesExtendsMapper;
-	
+
 	@Autowired
 	private CenEstadoSolicitudModifExtendsMapper cenEstadoSolicitudModifExtendsMapper;
-	
+
 	@Override
 	public ComboDTO getComboTipoModificacion(HttpServletRequest request) {
-	LOGGER.info("getComboTipoModificacion() -> Entrada al servicio para cargar el tipo de modificación");
-		
+		LOGGER.info("getComboTipoModificacion() -> Entrada al servicio para cargar el tipo de modificación");
+
 		// Conseguimos información del usuario logeado
 		String token = request.getHeader("Authorization");
 		String dni = UserTokenUtils.getDniFromJWTToken(token);
 		Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
 		ComboDTO combo = new ComboDTO();
-		
-		if(idInstitucion!= null){
+
+		if (idInstitucion != null) {
 			AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
 			exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
 			LOGGER.info(
@@ -50,16 +52,17 @@ public class SolicitudModificacionServiceImpl implements ISolicitudModificacionS
 			List<AdmUsuarios> usuarios = _admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
 			LOGGER.info(
 					"getComboTipoModificacion() / admUsuariosExtendsMapper.selectByExample() -> Salida de admUsuariosExtendsMapper para obtener información del usuario logeado");
-			
-			if(usuarios != null && usuarios.size()>0){
-				
+
+			if (usuarios != null && usuarios.size() > 0) {
+
 				AdmUsuarios usuario = usuarios.get(0);
 				LOGGER.info(
 						"getComboTipoModificacion() / cenTiposolicitudSqlExtendsMapper.selectTipoSolicitud() -> Entrada a cenTiposolicitudSqlExtendsMapper para obtener los tipos de modificación");
 
-				List<ComboItem> comboItems = cenTiposModificacionesExtendsMapper.getTipoModificacion(usuario.getIdlenguaje());
-				
-				if(comboItems != null && comboItems.size() >0){
+				List<ComboItem> comboItems = cenTiposModificacionesExtendsMapper
+						.getTipoModificacion(usuario.getIdlenguaje());
+
+				if (comboItems != null && comboItems.size() > 0) {
 					ComboItem element = new ComboItem();
 					element.setLabel("");
 					element.setValue("");
@@ -67,23 +70,23 @@ public class SolicitudModificacionServiceImpl implements ISolicitudModificacionS
 					combo.setCombooItems(comboItems);
 				}
 			}
-			
+
 		}
 		LOGGER.info("getComboTipoModificacion() -> Salida del servicio para obtener los tipos de modificación");
 		return combo;
 	}
-	
+
 	@Override
 	public ComboDTO getComboEstado(HttpServletRequest request) {
-	LOGGER.info("getComboEstado() -> Entrada al servicio para cargar los estados de solicitud");
-		
+		LOGGER.info("getComboEstado() -> Entrada al servicio para cargar los estados de solicitud");
+
 		// Conseguimos información del usuario logeado
 		String token = request.getHeader("Authorization");
 		String dni = UserTokenUtils.getDniFromJWTToken(token);
 		Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
 		ComboDTO combo = new ComboDTO();
-		
-		if(idInstitucion!= null){
+
+		if (idInstitucion != null) {
 			AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
 			exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
 			LOGGER.info(
@@ -91,16 +94,16 @@ public class SolicitudModificacionServiceImpl implements ISolicitudModificacionS
 			List<AdmUsuarios> usuarios = _admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
 			LOGGER.info(
 					"getComboEstado() / admUsuariosExtendsMapper.selectByExample() -> Salida de admUsuariosExtendsMapper para obtener información del usuario logeado");
-			
-			if(usuarios != null && usuarios.size()>0){
-				
+
+			if (usuarios != null && usuarios.size() > 0) {
+
 				AdmUsuarios usuario = usuarios.get(0);
 				LOGGER.info(
 						"getComboEstado() / cenTiposolicitudSqlExtendsMapper.selectTipoSolicitud() -> Entrada a cenTiposolicitudSqlExtendsMapper para obtener los estados de solicitud");
 
 				List<ComboItem> comboItems = cenEstadoSolicitudModifExtendsMapper.getEstado(usuario.getIdlenguaje());
-				
-				if(comboItems != null && comboItems.size() >0){
+
+				if (comboItems != null && comboItems.size() > 0) {
 					ComboItem element = new ComboItem();
 					element.setLabel("");
 					element.setValue("");
@@ -108,10 +111,49 @@ public class SolicitudModificacionServiceImpl implements ISolicitudModificacionS
 					combo.setCombooItems(comboItems);
 				}
 			}
-			
+
 		}
 		LOGGER.info("getComboEstado() -> Salida del servicio para obtener los estados de solicitud");
 		return combo;
+	}
+
+	@Override
+	public SolModificacionDTO searchModificationRequest(int numPagina,
+			SolicitudModificacionSearchDTO solicitudModificacionSearchDTO, HttpServletRequest request) {
+		LOGGER.info(
+				"searchModificationRequest() -> Entrada al servicio para recuperar las solicitudes de modificación");
+
+		// Conseguimos información del usuario logeado
+		String token = request.getHeader("Authorization");
+		String dni = UserTokenUtils.getDniFromJWTToken(token);
+		Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
+		SolModificacionDTO solModificacionDTO = new SolModificacionDTO();
+
+		if (idInstitucion != null) {
+			AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
+			exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
+			LOGGER.info(
+					"searchModificationRequest() / admUsuariosExtendsMapper.selectByExample() -> Entrada a admUsuariosExtendsMapper para obtener información del usuario logeado");
+			List<AdmUsuarios> usuarios = _admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
+			LOGGER.info(
+					"searchModificationRequest() / admUsuariosExtendsMapper.selectByExample() -> Salida de admUsuariosExtendsMapper para obtener información del usuario logeado");
+
+			if (usuarios != null && usuarios.size() > 0) {
+
+				AdmUsuarios usuario = usuarios.get(0);
+				LOGGER.info(
+						"datosSolicitudSearch() / cenEstadoSolicitudExtendsMapper.selectTipoSolicitud() -> Entrada a cenEstadoSolicitudExtendsMapper para obtener los estados de solicitud");
+				solicitudModificacionSearchDTO.setIdInstitucion(idInstitucion.toString());
+				List<SolModificacionItem> solModificacionItems = cenTiposModificacionesExtendsMapper.searchModificationRequest(solicitudModificacionSearchDTO, usuario.getIdlenguaje());
+				solModificacionDTO.setSolModificacionItems(solModificacionItems);
+
+			}
+		}
+
+		LOGGER.info(
+				"searchModificationRequest() -> Salida del servicio para recuperar las solicitudes de modificación");
+
+		return solModificacionDTO;
 	}
 
 }
