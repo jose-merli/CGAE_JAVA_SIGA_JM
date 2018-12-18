3455cfd24135c8ef9fd56dc404d304a8ad8f3f68
Contreras Egea, Noelia(ES - Madrid) <ncontrerasegea@deloitte.es>
2018-12-18 17:51:55 +0100

    Consultas

---
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/mappers/ConConsultasExtendsMapper.java
@@ -0,0 +1,42 @@
+package org.itcgae.siga.db.services.com.mappers;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.type.JdbcType;
+import org.itcgae.siga.DTOs.com.ConsultaItem;
+import org.itcgae.siga.DTOs.com.ConsultasSearch;
+import org.itcgae.siga.db.services.com.providers.ConConsultasExtendsSqlProvider;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public interface ConConsultasExtendsMapper {
+
+	@SelectProvider(type = ConConsultasExtendsSqlProvider.class, method = "selectConsultas")
+	@Results({ @Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "IDCONSULTA", property = "idConsulta", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "DESCRIPCION", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "GENERAL", property = "generica", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOCONSULTA", property = "tipoConsulta", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDMODULO", property = "idModulo", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "BASES", property = "bases", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTABLA", property = "idTabla", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "ESEXPERTA", property = "experta", jdbcType = JdbcType.VARCHAR),
+			// CLOB
+			// @Result(column = "SENTENCIA", property = "sentencia", jdbcType =
+			// JdbcType.VARCHAR),
+			@Result(column = "OBSERVACIONES", property = "descripcion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "FECHABAJA", property = "fechaBaja", jdbcType = JdbcType.DATE),
+			@Result(column = "IDOBJETIVO", property = "idObjetivo", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "IDCLASE", property = "idClase", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "NOMBREOBJETIVO", property = "objetivo", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "NOMBREMODULO", property = "modulo", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "NOMBRECLASE", property = "claseComunicacion", jdbcType = JdbcType.VARCHAR) })
+	List<ConsultaItem> selectConsultasSearch(Short idInstitucion, String idLenguaje, ConsultasSearch filtros);
+
+}
---
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ConConsultasExtendsSqlProvider.java
@@ -0,0 +1,72 @@
+package org.itcgae.siga.db.services.com.providers;
+
+import java.text.SimpleDateFormat;
+
+import org.apache.ibatis.jdbc.SQL;
+import org.itcgae.siga.DTOs.com.ConsultasSearch;
+
+public class ConConsultasExtendsSqlProvider {
+	
+public String selectConsultas(Short idInstitucion, String idLenguaje,ConsultasSearch filtros){
+		
+		SQL sql = new SQL();
+		// Formateo de fecha para sentencia sql
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+				
+		sql.SELECT("CONSULTA.IDINSTITUCION");
+		sql.SELECT("CONSULTA.IDCONSULTA");
+		sql.SELECT("CONSULTA.DESCRIPCION");
+		sql.SELECT("DECODE(CONSULTA.GENERAL,'1','Si','S','Si','0','No','N','No',CONSULTA.GENERAL) AS GENERAL");
+		sql.SELECT("CONSULTA.TIPOCONSULTA");
+		sql.SELECT("CONSULTA.IDMODULO");
+		sql.SELECT("(SELECT MODULO.NOMBRE FROM CON_MODULO MODULO WHERE MODULO.IDMODULO=CONSULTA.IDMODULO) AS NOMBREMODULO");
+		sql.SELECT("CONSULTA.FECHAMODIFICACION");
+		sql.SELECT("CONSULTA.USUMODIFICACION");
+		sql.SELECT("CONSULTA.BASES");
+		sql.SELECT("CONSULTA.IDTABLA");
+		sql.SELECT("CONSULTA.ESEXPERTA");
+		sql.SELECT("CONSULTA.OBSERVACIONES");
+		sql.SELECT("CONSULTA.FECHABAJA");
+		sql.SELECT("CONSULTA.IDOBJETIVO");
+		sql.SELECT("(SELECT DISTINCT rec.DESCRIPCION FROM CON_OBJETIVO objetivo INNER JOIN GEN_RECURSOS_CATALOGOS rec ON (rec.IDRECURSO = objetivo.NOMBRE AND rec.IDLENGUAJE = '1') WHERE objetivo.IDOBJETIVO=CONSULTA.IDOBJETIVO) AS NOMBREOBJETIVO");
+		sql.SELECT("CONSULTA.IDCLASE");
+		sql.SELECT("CONSULTA.IDCLASECOMUNICACION");
+		sql.SELECT("(SELECT CLASE.NOMBRE  FROM MOD_CLASECOMUNICACIONES CLASE WHERE CLASE.IDCLASECOMUNICACION=CONSULTA.IDCLASECOMUNICACION) AS NOMBRECLASE");
+		sql.FROM("CON_CONSULTA CONSULTA");
+		
+		sql.WHERE("CONSULTA.IDINSTITUCION = '" + idInstitucion +"'");
+		sql.WHERE("CONSULTA.FECHABAJA IS NULL");
+		
+		if(filtros.getNombre() != null && !filtros.getNombre().trim().equals("")){
+			sql.WHERE(filtroTextoBusquedas("CONSULTA.DESCRIPCION",filtros.getNombre()));
+		}
+		if(filtros.getDescripcion() != null && !filtros.getDescripcion().trim().equals("")){
+			sql.WHERE(filtroTextoBusquedas("CONSULTA.OBSERVACIONES", filtros.getDescripcion()));
+		}
+		
+		if(filtros.getIdModulo() != null && !filtros.getIdModulo().trim().equals("")){
+			sql.WHERE("CONSULTA.IDMODULO = '" + filtros.getIdModulo() +"'");
+		}
+		
+		if(filtros.getGenerica() != null && !filtros.getGenerica().trim().equals("")){
+			sql.WHERE("CONSULTA.GENERAL = '" + filtros.getGenerica() +"'");
+		}
+		if(filtros.getIdClaseComunicacion() != null && !filtros.getIdClaseComunicacion().trim().equals("")){
+			sql.WHERE("CONSULTA.IDCLASECOMUNICACION = '" + filtros.getIdClaseComunicacion() +"'");
+		}
+		if(filtros.getIdObjetivo() != null && !filtros.getIdObjetivo().trim().equals("")){
+			sql.WHERE("CONSULTA.IDOBJETIVO = '" + filtros.getIdObjetivo() +"'");
+		}
+		return sql.toString();
+	}
+
+public static String filtroTextoBusquedas(String columna, String cadena) {
+
+	StringBuilder cadenaWhere = new StringBuilder();
+	cadenaWhere.append(" (TRANSLATE(LOWER( " + columna + "),'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž','AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz') ");
+	cadenaWhere.append(" LIKE");
+	cadenaWhere.append(" TRANSLATE(LOWER('%" + cadena + "%'),'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž','AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz')) ");
+	return cadenaWhere.toString();
+	
+} 
+}
