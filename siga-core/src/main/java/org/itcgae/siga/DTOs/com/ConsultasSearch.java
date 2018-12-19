9b415e5989e526daefb1be3b27c2b8410235b0c8
Contreras Egea, Noelia(ES - Madrid) <ncontrerasegea@deloitte.es>
2018-12-19 09:20:57 +0100

    Consulta Item

--- siga-core/src/main/java/org/itcgae/siga/DTOs/com/ConsultasSearch.java
+++ siga-core/src/main/java/org/itcgae/siga/DTOs/com/ConsultasSearch.java
@@ -1,5 +1,93 @@
 package org.itcgae.siga.DTOs.com;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ConsultasSearch {
 
+	private String nombre;
+	private String descripcion;
+	private String modulo;
+	private String generica;
+	private String objetivo;
+	private String claseComunicacion;
+	private String idModulo;
+	private String idObjetivo;
+	private String IdClaseComunicacion;
+	
+	
+	@JsonProperty("idModulo")
+	public String getIdModulo() {
+		return idModulo;
+	}
+	public void setIdModulo(String idModulo) {
+		this.idModulo = idModulo;
+	}
+	
+	@JsonProperty("idObjetivo")
+	public String getIdObjetivo() {
+		return idObjetivo;
+	}
+	public void setIdObjetivo(String idObjetivo) {
+		this.idObjetivo = idObjetivo;
+	}
+	
+	@JsonProperty("IdClaseComunicacion")
+	public String getIdClaseComunicacion() {
+		return IdClaseComunicacion;
+	}
+	public void setIdClaseComunicacion(String idClaseComunicacion) {
+		IdClaseComunicacion = idClaseComunicacion;
+	}
+	@JsonProperty("nombre")
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	@JsonProperty("descripcion")
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	@JsonProperty("modulo")
+	public String getModulo() {
+		return modulo;
+	}
+	public void setModulo(String modulo) {
+		this.modulo = modulo;
+	}
+	
+	@JsonProperty("generica")
+	public String getGenerica() {
+		return generica;
+	}
+	public void setGenerica(String generica) {
+		this.generica = generica;
+	}
+	
+	@JsonProperty("objetivo")
+	public String getObjetivo() {
+		return objetivo;
+	}
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+	
+	@JsonProperty("claseComunicacion")
+	public String getClaseComunicacion() {
+		return claseComunicacion;
+	}
+	public void setClaseComunicacion(String claseComunicacion) {
+		this.claseComunicacion = claseComunicacion;
+	}
+	
+	
+	
 }
