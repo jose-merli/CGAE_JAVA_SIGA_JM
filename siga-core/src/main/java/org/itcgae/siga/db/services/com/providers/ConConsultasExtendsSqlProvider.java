a1ddda94104929fee8a5b24ddbb138354181b7fe
Salinero, Isabel (ES - Madrid) <isalinero@deloitte.es>
2019-01-03 18:58:17 +0100

    cambio en sql consultas

--- siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ConConsultasExtendsSqlProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ConConsultasExtendsSqlProvider.java
@@ -57,9 +57,9 @@ public String selectConsultas(Short idInstitucion, String idLenguaje,ConsultasSe
 		
 		if(filtros.getGenerica() != null && !filtros.getGenerica().trim().equals("")){
 			if(filtros.getGenerica().equals("N")){
-				sql.WHERE("(CONSULTA.GENERAL = 'N' OR CONSULTA.GENERAL = 'n'OR CONSULTA.GENERAL = '0' AND CONSULTA.IDINSTITUCION = '" + idInstitucion +"')");
+				sql.WHERE("(CONSULTA.GENERAL = 'N' OR CONSULTA.GENERAL = 'n' OR CONSULTA.GENERAL = '0' AND CONSULTA.IDINSTITUCION = '" + idInstitucion +"')");
 			}else{
-				sql.WHERE("(CONSULTA.GENERAL = 'S' OR CONSULTA.GENERAL = 's' OR  CONSULTA.GENERAL = '1' AND CONSULTA.IDINSTITUCION = '2000' OR CONSULTA.IDINSTITUCION = '" + idInstitucion +"')");
+				sql.WHERE("(CONSULTA.GENERAL = 'S' OR CONSULTA.GENERAL = 's' OR  CONSULTA.GENERAL = '1' AND (CONSULTA.IDINSTITUCION = '2000' OR CONSULTA.IDINSTITUCION = '" + idInstitucion +"'))");
 			}
 		}
 		
