a97a1cbae0fc65c538476e698dcc8a609ba730a7
María José Tubío <mtubio@deloitte.es>
2019-01-17 18:11:53 +0100

    [mtubio] Scripts Formación y Agenda

---
+++ siga-core/src/main/resources/bd/FORMACIÓN/20190118/20. TRADUCCIONES FORMACION.sql
@@ -0,0 +1,72 @@
+﻿Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.importe','Importe','0','1',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.importe','Importe#GL','0','4',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.importe','Import','0','2',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.importe','Importe#EU','0','3',to_date('08/01/19','DD/MM/RR'),'0','19');
+                                                                                                                                      
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.periocidad','Periocidad','0','1',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.periocidad','Periocidad#GL','0','4',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.periocidad','Periodicitat','0','2',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.periocidad','Periocidad#EU','0','3',to_date('08/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precioPorDefecto','Precio por defecto','0','1',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precioPorDefecto','Precio por defecto#GL','0','4',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precioPorDefecto','Preu per defecte','0','2',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precioPorDefecto','Precio por defecto#EU','0','3',to_date('08/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precios','Precios','0','1',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precios','Precios#GL','0','4',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precios','Preus','0','2',to_date('08/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaPrecios.precios','Precios#EU','0','3',to_date('08/01/19','DD/MM/RR'),'0','19');
+																																
+
+commit;
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioInscripcion','Fecha Inicio Inscripción','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioInscripcion','Data Inici Inscripció','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioInscripcion','Fecha Inicio Inscripción#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioInscripcion','Fecha Inicio Inscripción#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinInscripcion','Fecha Fin Inscripción','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinInscripcion','Data Fi Inscripció','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinInscripcion','Fecha Fin Inscripción#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinInscripcion','Fecha Fin Inscripción#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioImparticion','Fecha Inicio Impartición','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioImparticion','Data Inici Impartició','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioImparticion','Fecha Inicio Impartición#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaInicioImparticion','Fecha Inicio Impartición#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinImparticion','Fecha Fin Impartición','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinImparticion','Data Fi Impartició','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinImparticion','Fecha Fin Impartición#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.fechaFinImparticion','Fecha Fin Impartición#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.boton.inscripcion','Inscripción','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.boton.inscripcion','Inscripció','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.boton.inscripcion','Inscripción#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaDatosGestion.boton.inscripcion','Inscripción#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.inscripciones.cabecera','Inscripciones','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.inscripciones.cabecera','Inscripcions','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.inscripciones.cabecera','Inscripciones#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.inscripciones.cabecera','Inscripciones#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.pendientes','Pendientes','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.pendientes','Pendents','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.pendientes','Pendientes#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.pendientes','Pendientes#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.aprobadas','Aprobadas','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.aprobadas','Aprobadas','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.aprobadas','Aprobadas#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.aprobadas','Aprobadas#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.canceladas','Canceladas','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.canceladas','Cancel·lades','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.canceladas','Canceladas#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.canceladas','Canceladas#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.rechazadas','Rechazadas','0','1',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.rechazadas','Rebutjades','0','2',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.rechazadas','Rechazadas#GL','0','4',to_date('10/01/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('formacion.fichaCurso.tarjetaInscripciones.rechazadas','Rechazadas#EU','0','3',to_date('10/01/19','DD/MM/RR'),'0','19');
