e561d5bf998fff2986a0d6e6a028504354619046
Borja Herrero <bherrero@deloitte.es>
2019-01-29 11:50:08 +0100

    nombre clase comunicacion y modelo

--- siga-core/src/main/java/org/itcgae/siga/DTOs/com/EnviosMasivosItem.java
+++ siga-core/src/main/java/org/itcgae/siga/DTOs/com/EnviosMasivosItem.java
@@ -25,6 +25,7 @@ public class EnviosMasivosItem {
 	private String idClaseComunicacion;
 	private String modeloComunicacion;
 	private String claseComunicacion;
+	
 	public Short getIdInstitucion() {
 		return idInstitucion;
 	}
