9d0516b9eaf13860e4185a68d834235d69244287
María José Tubío <mtubio@deloitte.es>
2019-04-01 17:54:25 +0200

    [mtubio] Informes y comunicaciones -- Incidencias SIGARNV-596 y SIGARNV-603

--- siga-core/src/main/java/org/itcgae/siga/db/services/com/mappers/ModModeloComunicacionExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/mappers/ModModeloComunicacionExtendsMapper.java
@@ -19,87 +19,82 @@ import org.springframework.stereotype.Service;
 @Service
 @Primary
 public interface ModModeloComunicacionExtendsMapper {
-	
+
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectModelosComunicacion")
-	@Results({@Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "VISIBLE", property = "visible", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "ORDEN", property = "orden", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "DESCRIPCION", property = "descripcion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "PRESELECCIONAR", property = "preseleccionar", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "NOMBRECLASE", property = "claseComunicacion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "ABREVIATURA", property = "institucion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "FECHABAJA", property = "fechaBaja", jdbcType = JdbcType.DATE),
-		@Result(column = "PORDEFECTO", property = "porDefecto", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR)
-	})
-	List<ModelosComunicacionItem> selectModelosComunicacion(String idInstitucion, DatosModelosComunicacionesSearch filtros, String idLenguaje, boolean historico);
-	
+	@Results({ @Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "VISIBLE", property = "visible", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ORDEN", property = "orden", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "DESCRIPCION", property = "descripcion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "PRESELECCIONAR", property = "preseleccionar", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "NOMBRECLASE", property = "claseComunicacion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ABREVIATURA", property = "institucion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "FECHABAJA", property = "fechaBaja", jdbcType = JdbcType.DATE),
+			@Result(column = "PORDEFECTO", property = "porDefecto", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR) })
+	List<ModelosComunicacionItem> selectModelosComunicacion(String idInstitucion,
+			DatosModelosComunicacionesSearch filtros, String idLenguaje, boolean historico);
+
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectModelo")
-	@Results({@Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "VISIBLE", property = "visible", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "ORDEN", property = "orden", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "DESCRIPCION", property = "descripcion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "PRESELECCIONAR", property = "preseleccionar", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.NUMERIC),
-		@Result(column = "NOMBRECLASE", property = "claseComunicacion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "ABREVIATURA", property = "institucion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "FECHABAJA", property = "fechaBaja", jdbcType = JdbcType.DATE),
-		@Result(column = "PORDEFECTO", property = "porDefecto", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR)
-	})
+	@Results({ @Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "VISIBLE", property = "visible", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ORDEN", property = "orden", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "DESCRIPCION", property = "descripcion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "PRESELECCIONAR", property = "preseleccionar", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.NUMERIC),
+			@Result(column = "NOMBRECLASE", property = "claseComunicacion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ABREVIATURA", property = "institucion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "FECHABAJA", property = "fechaBaja", jdbcType = JdbcType.DATE),
+			@Result(column = "PORDEFECTO", property = "porDefecto", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR) })
 	ModelosComunicacionItem selectModelo(String idModelo, String idLenguaje);
 
-	
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectModelosComunicacionDialg")
-	@Results({@Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR)
-	})
-	List<ModelosComunicacionItem> selectModelosComunicacionDialogo(String idInstitucion, String idClaseComunicacion, String idModulo, String idLenguaje, String idConsulta, List<String> perfiles);
+	@Results({ @Result(column = "IDMODELOCOMUNICACION", property = "idModeloComunicacion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDCLASECOMUNICACION", property = "idClaseComunicacion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTIPOENVIOS", property = "idTipoEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "PRESELECCIONAR", property = "preseleccionar", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "VISIBLE", property = "visible", jdbcType = JdbcType.VARCHAR)
 
-	
-	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectModelosClasesComunicacion")
-	@Results({@Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR)
 	})
+	List<ModelosComunicacionItem> selectModelosComunicacionDialogo(String idInstitucion, String idClaseComunicacion,
+			String idModulo, String idLenguaje, String idConsulta, List<String> perfiles);
+
+	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectModelosClasesComunicacion")
+	@Results({ @Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR) })
 	List<ComboItem> selectModelosClasesComunicacion(String idClasesComunicacion);
 
-	
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectPlantillaModelo")
-	@Results({@Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR)
-	})
+	@Results({ @Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR) })
 	List<ComboItem> selectPlantillasModelos(String idModelo, Short idInstitucion);
-	
+
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectTipoEnvioPlantilla")
-	@Results({@Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR)
-	})
+	@Results({ @Result(column = "VALUE", property = "value", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "LABEL", property = "label", jdbcType = JdbcType.VARCHAR) })
 	List<ComboItem> selectTipoEnvioPlantilla(String idLenguaje, String idPlantilla);
 
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "comprobarNombreDuplicado")
-	@Results({ @Result(column = "NOMBRE", property = "valor", jdbcType = JdbcType.VARCHAR) 
-	})
+	@Results({ @Result(column = "NOMBRE", property = "valor", jdbcType = JdbcType.VARCHAR) })
 	StringDTO comprobarNombreDuplicado(String nombreModelo);
 
 	@SelectProvider(type = ModModeloComunicacionExtendsSqlProvider.class, method = "selectPlantillaPorDefecto")
-	@Results({@Result(column = "NOMBRE", property = "nombrePlantilla", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDTIPOENVIOS", property = "idTipoEnvios", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvios", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.VARCHAR)
-	})
+	@Results({ @Result(column = "NOMBRE", property = "nombrePlantilla", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTIPOENVIOS", property = "idTipoEnvios", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "TIPOENVIO", property = "tipoEnvio", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDPLANTILLAENVIOS", property = "idPlantillaEnvios", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.VARCHAR) })
 	List<PlantillaModeloItem> selectPlantillaPorDefecto(String idModelo, Short idInstitucion, String idLenguaje);
 }
--- siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ModModeloComunicacionExtendsSqlProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ModModeloComunicacionExtendsSqlProvider.java
@@ -11,14 +11,15 @@ import org.itcgae.siga.DTOs.com.DatosModelosComunicacionesSearch;
 import org.itcgae.siga.commons.constants.SigaConstants;
 
 public class ModModeloComunicacionExtendsSqlProvider {
-	
-	public String selectModelosComunicacion(String idInstitucion, DatosModelosComunicacionesSearch filtros, String idLenguaje, boolean historico){
-		
+
+	public String selectModelosComunicacion(String idInstitucion, DatosModelosComunicacionesSearch filtros,
+			String idLenguaje, boolean historico) {
+
 		SQL sql = new SQL();
-		
+
 		// Formateo de fecha para sentencia sql
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-				
+
 		sql.SELECT("modelo.IDMODELOCOMUNICACION");
 		sql.SELECT("modelo.NOMBRE");
 		sql.SELECT("modelo.VISIBLE");
@@ -34,53 +35,55 @@ public class ModModeloComunicacionExtendsSqlProvider {
 		sql.SELECT("modelo.PORDEFECTO");
 		sql.SELECT("modelo.IDPLANTILLAENVIOS");
 		sql.SELECT("modelo.IDTIPOENVIOS");
-		sql.SELECT("(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = modelo.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"+ idLenguaje +"') AS TIPOENVIO");
-		
+		sql.SELECT(
+				"(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = modelo.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"
+						+ idLenguaje + "') AS TIPOENVIO");
+
 		sql.FROM("MOD_MODELOCOMUNICACION modelo");
-		
+
 		sql.INNER_JOIN("MOD_CLASECOMUNICACIONES clase ON modelo.IDCLASECOMUNICACION = clase.IDCLASECOMUNICACION");
 		sql.INNER_JOIN("CEN_INSTITUCION inst ON inst.IDINSTITUCION = modelo.IDINSTITUCION");
-		
-		if(filtros.getIdInstitucion() != null && !filtros.getIdInstitucion().trim().equals("")){
-			if(filtros.getIdInstitucion().equalsIgnoreCase(SigaConstants.IDINSTITUCION_0)){
+
+		if (filtros.getIdInstitucion() != null && !filtros.getIdInstitucion().trim().equals("")) {
+			if (filtros.getIdInstitucion().equalsIgnoreCase(SigaConstants.IDINSTITUCION_0)) {
 				sql.WHERE("(modelo.IDINSTITUCION = '2000' AND modelo.PORDEFECTO = 'SI')");
-			}else if(filtros.getIdInstitucion().equalsIgnoreCase(String.valueOf(SigaConstants.IDINSTITUCION_2000))){
+			} else if (filtros.getIdInstitucion().equalsIgnoreCase(String.valueOf(SigaConstants.IDINSTITUCION_2000))) {
 				sql.WHERE("(modelo.IDINSTITUCION = '2000' AND modelo.PORDEFECTO = 'NO')");
-			}else{
-				sql.WHERE("modelo.IDINSTITUCION = '"+filtros.getIdInstitucion()+"'");
-			}			
+			} else {
+				sql.WHERE("modelo.IDINSTITUCION = '" + filtros.getIdInstitucion() + "'");
+			}
 		}
-		if(filtros.getIdClaseComunicacion() != null && !filtros.getIdClaseComunicacion().trim().equals("")){
-			sql.WHERE("modelo.IDCLASECOMUNICACION = '"+filtros.getIdClaseComunicacion()+"'");
+		if (filtros.getIdClaseComunicacion() != null && !filtros.getIdClaseComunicacion().trim().equals("")) {
+			sql.WHERE("modelo.IDCLASECOMUNICACION = '" + filtros.getIdClaseComunicacion() + "'");
 		}
-		
-		if(filtros.getNombre() != null && !filtros.getNombre().trim().equals("")){
-			sql.WHERE("lower(modelo.NOMBRE) LIKE lower('%"+filtros.getNombre()+"%')");
-		}
-		if(filtros.getPreseleccionar() != null && !filtros.getPreseleccionar().trim().equals("")){
-			sql.WHERE("modelo.PRESELECCIONAR = '"+filtros.getPreseleccionar()+"'");	
-		}
-		if(filtros.getVisible() != null && !filtros.getVisible().trim().equals("")){
-			sql.WHERE("modelo.VISIBLE = '"+filtros.getVisible()+"'");	
-		}
-		
 
+		if (filtros.getNombre() != null && !filtros.getNombre().trim().equals("")) {
+			sql.WHERE("lower(modelo.NOMBRE) LIKE lower('%" + filtros.getNombre() + "%')");
+		}
+		if (filtros.getPreseleccionar() != null && !filtros.getPreseleccionar().trim().equals("")) {
+			sql.WHERE("modelo.PRESELECCIONAR = '" + filtros.getPreseleccionar() + "'");
+		}
+		if (filtros.getVisible() != null && !filtros.getVisible().trim().equals("")) {
+			sql.WHERE("modelo.VISIBLE = '" + filtros.getVisible() + "'");
+		}
 
-		sql.WHERE("(modelo.IDINSTITUCION = '"+ idInstitucion +"' OR (modelo.IDINSTITUCION = '2000' AND modelo.PORDEFECTO = 'SI'))");
+		sql.WHERE("(modelo.IDINSTITUCION = '" + idInstitucion
+				+ "' OR (modelo.IDINSTITUCION = '2000' AND modelo.PORDEFECTO = 'SI'))");
 
-		
-		if(historico){
-			if(filtros.getFechaBaja() != null){
+		if (historico) {
+			if (filtros.getFechaBaja() != null) {
 				String fechaBaja = dateFormat.format(filtros.getFechaBaja());
 				String fechaBaja2 = dateFormat.format(filtros.getFechaBaja());
 				fechaBaja += " 00:00:00";
 				fechaBaja2 += " 23:59:59";
-				sql.WHERE("(modelo.FECHABAJA >= TO_DATE('" +fechaBaja + "', 'DD/MM/YYYY HH24:MI:SS') AND modelo.FECHABAJA <= TO_DATE('" +fechaBaja2 + "', 'DD/MM/YYYY HH24:MI:SS'))");
+				sql.WHERE("(modelo.FECHABAJA >= TO_DATE('" + fechaBaja
+						+ "', 'DD/MM/YYYY HH24:MI:SS') AND modelo.FECHABAJA <= TO_DATE('" + fechaBaja2
+						+ "', 'DD/MM/YYYY HH24:MI:SS'))");
 			}
-		}else{
+		} else {
 			sql.WHERE("modelo.FECHABAJA is NULL");
 		}
-		
+
 		if (historico) {
 			sql.ORDER_BY("modelo.FECHABAJA ASC, IDMODELOCOMUNICACION ASC");
 		} else {
@@ -89,86 +92,94 @@ public class ModModeloComunicacionExtendsSqlProvider {
 
 		return sql.toString();
 	}
-	
-		
-	public String selectModelosComunicacionDialg(String idInstitucion, String idClaseComunicacion, String idModulo, String idLenguaje, String idConsulta, List<String> perfiles){
-	   
+
+	public String selectModelosComunicacionDialg(String idInstitucion, String idClaseComunicacion, String idModulo,
+			String idLenguaje, String idConsulta, List<String> perfiles) {
+
 		SQL sql = new SQL();
-		   
-	   sql.SELECT("MODELO.IDCLASECOMUNICACION, MODELO.IDMODELOCOMUNICACION, MODELO.NOMBRE");
-	   sql.SELECT("MODELO.IDPLANTILLAENVIOS");
-	   sql.SELECT("MODELO.IDTIPOENVIOS");
-	   sql.SELECT("(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = MODELO.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"+ idLenguaje +"') AS TIPOENVIO");
+
+		sql.SELECT("MODELO.IDCLASECOMUNICACION, MODELO.IDMODELOCOMUNICACION, MODELO.NOMBRE");
+		sql.SELECT("MODELO.IDPLANTILLAENVIOS");
+		sql.SELECT("MODELO.IDTIPOENVIOS");
+		sql.SELECT("MODELO.VISIBLE");
+		sql.SELECT("MODELO.PRESELECCIONAR");
+		sql.SELECT(
+				"(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = MODELO.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"
+						+ idLenguaje + "') AS TIPOENVIO");
+
+		sql.FROM("MOD_MODELOCOMUNICACION MODELO");
+		sql.JOIN("MOD_CLASECOMUNICACIONES CLASE ON CLASE.IDCLASECOMUNICACION = MODELO.IDCLASECOMUNICACION");
+		sql.JOIN(
+				"MOD_MODELO_PERFILES PERFILES ON PERFILES.IDMODELOCOMUNICACION = MODELO.IDMODELOCOMUNICACION AND PERFILES.IDINSTITUCION = MODELO.IDINSTITUCION");
+
+		sql.WHERE("MODELO.IDCLASECOMUNICACION = " + idClaseComunicacion + " AND (CLASE.IDMODULO = " + idModulo
+				+ " OR CLASE.IDMODULO IS NULL)");
+
+		if (idConsulta != null) {
+			sql.WHERE(
+					"MODELO.IDMODELOCOMUNICACION IN (SELECT IDMODELOCOMUNICACION FROM MOD_PLANTILLADOC_CONSULTA WHERE IDINSTITUCION='"
+							+ idInstitucion + "' AND IDCONSULTA='" + idConsulta + "' AND FECHABAJA IS NULL)");
+		}
+
+		if (perfiles != null && perfiles.size() > 0) {
+			sql.WHERE("PERFILES.IDPERFIL IN (" + String.join(",", perfiles) + ")");
+		}
 		
-	   sql.FROM("MOD_MODELOCOMUNICACION MODELO");
-	   sql.JOIN("MOD_CLASECOMUNICACIONES CLASE ON CLASE.IDCLASECOMUNICACION = MODELO.IDCLASECOMUNICACION");
-	   sql.JOIN("MOD_MODELO_PERFILES PERFILES ON PERFILES.IDMODELOCOMUNICACION = MODELO.IDMODELOCOMUNICACION AND PERFILES.IDINSTITUCION = MODELO.IDINSTITUCION");
-	      
-	   sql.WHERE("MODELO.IDCLASECOMUNICACION = " + idClaseComunicacion + " AND (CLASE.IDMODULO = " + idModulo + " OR CLASE.IDMODULO IS NULL)");
-	   
-	   if(idConsulta != null) {
-		   sql.WHERE("MODELO.IDMODELOCOMUNICACION IN (SELECT IDMODELOCOMUNICACION FROM MOD_PLANTILLADOC_CONSULTA WHERE IDINSTITUCION='"+idInstitucion+"' AND IDCONSULTA='"+idConsulta+"' AND FECHABAJA IS NULL)");
-	   }
-	   
-	   if(perfiles != null && perfiles.size() > 0){
-		   sql.WHERE("PERFILES.IDPERFIL IN (" + String.join(",", perfiles) + ")");
-	   }
-	   
-	   sql.ORDER_BY("MODELO.ORDEN");
-	   
-	   return sql.toString();
+		sql.WHERE("MODELO.VISIBLE = '1'");
+		sql.ORDER_BY("MODELO.ORDEN");
+
+		return sql.toString();
 	}
-	
-	public String selectPlantillaModelo(String idModeloComunicacion, Short idInstitucion){
-		
+
+	public String selectPlantillaModelo(String idModeloComunicacion, Short idInstitucion) {
+
 		SQL sql = new SQL();
 		sql.SELECT("MPLANTILLA.IDPLANTILLAENVIOS AS VALUE, PLANTILLA.NOMBRE AS LABEL");
 		sql.FROM("MOD_MODELO_PLANTILLAENVIO MPLANTILLA");
-		sql.JOIN("ENV_PLANTILLASENVIOS PLANTILLA ON PLANTILLA.IDPLANTILLAENVIOS = MPLANTILLA.IDPLANTILLAENVIOS AND MPLANTILLA.IDINSTITUCION = PLANTILLA.IDINSTITUCION AND MPLANTILLA.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS");
-		sql.WHERE("MPLANTILLA.IDMODELOCOMUNICACION = '"+ idModeloComunicacion +"'");
+		sql.JOIN(
+				"ENV_PLANTILLASENVIOS PLANTILLA ON PLANTILLA.IDPLANTILLAENVIOS = MPLANTILLA.IDPLANTILLAENVIOS AND MPLANTILLA.IDINSTITUCION = PLANTILLA.IDINSTITUCION AND MPLANTILLA.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS");
+		sql.WHERE("MPLANTILLA.IDMODELOCOMUNICACION = '" + idModeloComunicacion + "'");
 		sql.WHERE("PLANTILLA.IDINSTITUCION = " + idInstitucion + " AND MPLANTILLA.FECHABAJA IS NULL");
-		   
+
 		return sql.toString();
 	}
-	
-	
-	public String selectTipoEnvioPlantilla(String idLenguaje, String idPlantilla){
-		
+
+	public String selectTipoEnvioPlantilla(String idLenguaje, String idPlantilla) {
+
 		SQL sql = new SQL();
 		sql.SELECT("TIPO.IDTIPOENVIOS AS VALUE, CAT.DESCRIPCION");
 		sql.FROM("ENV_TIPOENVIOS TIPO");
 		sql.JOIN("GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = TIPO.NOMBRE AND CAT.IDLENGUAJE = '" + idLenguaje + "'");
-		sql.WHERE("TIPO.IDTIPOENVIOS = '" + idPlantilla + "'");		
-		
+		sql.WHERE("TIPO.IDTIPOENVIOS = '" + idPlantilla + "'");
+
 		return sql.toString();
 	}
 
-	public String selectModelosClasesComunicacion(String idClasesComunicacion){
-		   
+	public String selectModelosClasesComunicacion(String idClasesComunicacion) {
+
 		SQL sql = new SQL();
-	
+
 		sql.SELECT("MODELO.IDMODELOCOMUNICACION as value, MODELO.NOMBRE as label");
 		sql.FROM("MOD_MODELOCOMUNICACION MODELO");
-		sql.WHERE("MODELO.IDCLASECOMUNICACION IN ("+idClasesComunicacion+")");
-			   
+		sql.WHERE("MODELO.IDCLASECOMUNICACION IN (" + idClasesComunicacion + ")");
+
 		return sql.toString();
 	}
-	
-	
-	public String comprobarNombreDuplicado(String nombreModelo){
-		
+
+	public String comprobarNombreDuplicado(String nombreModelo) {
+
 		SQL sql = new SQL();
 		sql.SELECT("max((nvl(SUBSTR(nombre,LENGTH('" + nombreModelo + "')+1,LENGTH(nombre)),0))+1) as NOMBRE");
 		sql.FROM("mod_modelocomunicacion");
 		sql.WHERE("lower(nombre) like lower('" + nombreModelo + "%')");
-		   
+
 		return sql.toString();
 	}
 
-	public String selectModelo(String idModelo, String idLenguaje){
-		
+	public String selectModelo(String idModelo, String idLenguaje) {
+
 		SQL sql = new SQL();
-						
+
 		sql.SELECT("modelo.IDMODELOCOMUNICACION");
 		sql.SELECT("modelo.NOMBRE");
 		sql.SELECT("modelo.VISIBLE");
@@ -184,31 +195,36 @@ public class ModModeloComunicacionExtendsSqlProvider {
 		sql.SELECT("modelo.PORDEFECTO");
 		sql.SELECT("modelo.IDPLANTILLAENVIOS");
 		sql.SELECT("modelo.IDTIPOENVIOS");
-		sql.SELECT("(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = modelo.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"+ idLenguaje +"') AS TIPOENVIO");
-		
+		sql.SELECT(
+				"(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = modelo.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"
+						+ idLenguaje + "') AS TIPOENVIO");
+
 		sql.FROM("MOD_MODELOCOMUNICACION modelo");
-		
+
 		sql.INNER_JOIN("MOD_CLASECOMUNICACIONES clase ON modelo.IDCLASECOMUNICACION = clase.IDCLASECOMUNICACION");
 		sql.INNER_JOIN("CEN_INSTITUCION inst ON inst.IDINSTITUCION = modelo.IDINSTITUCION");
-		
-		sql.WHERE("modelo.IDMODELOCOMUNICACION = '"+ idModelo +"'");
+
+		sql.WHERE("modelo.IDMODELOCOMUNICACION = '" + idModelo + "'");
 		sql.WHERE("modelo.FECHABAJA is NULL");
 
 		sql.ORDER_BY("IDMODELOCOMUNICACION ASC");
 
 		return sql.toString();
 	}
-	
-	public String selectPlantillaPorDefecto(String idModelo, Short idInstitucion, String idLenguaje){
-		
+
+	public String selectPlantillaPorDefecto(String idModelo, Short idInstitucion, String idLenguaje) {
+
 		SQL sql = new SQL();
-		
+
 		sql.SELECT("PLANTILLA.NOMBRE, PLANTILLA.IDTIPOENVIOS, MODELO.IDINSTITUCION,"
-				+ "(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"+ idLenguaje +"') AS TIPOENVIO");
+				+ "(SELECT CAT.DESCRIPCION FROM ENV_TIPOENVIOS PLA LEFT JOIN GEN_RECURSOS_CATALOGOS CAT ON CAT.IDRECURSO = PLA.NOMBRE WHERE PLA.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS AND CAT.IDLENGUAJE = '"
+				+ idLenguaje + "') AS TIPOENVIO");
 		sql.SELECT("PLANTILLA.IDPLANTILLAENVIOS");
 		sql.FROM("MOD_MODELO_PLANTILLAENVIO MODELO");
-		sql.JOIN("ENV_PLANTILLASENVIOS PLANTILLA ON PLANTILLA.IDPLANTILLAENVIOS = MODELO.IDPLANTILLAENVIOS AND PLANTILLA.IDINSTITUCION = '"+ idInstitucion +"' AND MODELO.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS");
-		sql.WHERE("MODELO.IDMODELOCOMUNICACION = '" + idModelo +"'");
+		sql.JOIN(
+				"ENV_PLANTILLASENVIOS PLANTILLA ON PLANTILLA.IDPLANTILLAENVIOS = MODELO.IDPLANTILLAENVIOS AND PLANTILLA.IDINSTITUCION = '"
+						+ idInstitucion + "' AND MODELO.IDTIPOENVIOS = PLANTILLA.IDTIPOENVIOS");
+		sql.WHERE("MODELO.IDMODELOCOMUNICACION = '" + idModelo + "'");
 
 		return sql.toString();
 	}
