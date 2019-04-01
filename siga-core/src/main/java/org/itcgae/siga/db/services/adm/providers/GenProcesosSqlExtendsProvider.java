35ed5c77e4c21810b2ca87492a5a0b312a8fbaf2
Jesús Colchón Corbacho <jcolchon@deloitte.es>
2019-04-01 09:44:46 +0200

    jcolchon, incidencia de hiddens, y script para catálogos de roles y temas

--- siga-core/src/main/java/org/itcgae/siga/db/services/adm/providers/GenProcesosSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/adm/providers/GenProcesosSqlExtendsProvider.java
@@ -9,7 +9,7 @@ public class GenProcesosSqlExtendsProvider extends AdmContadorSqlProvider{
 
 	
 	
-	public String getProcesosPermisos(PermisoRequestItem request){
+	public String getProcesosPermisos(PermisoRequestItem request,String idInstitucionCert){
 		SQL sql = new SQL();
 
 		sql.SELECT_DISTINCT("PROC.IDPROCESO AS ID");
@@ -21,7 +21,7 @@ public class GenProcesosSqlExtendsProvider extends AdmContadorSqlProvider{
 		
 		sql.LEFT_OUTER_JOIN("ADM_TIPOSACCESO ACCESO ON (PROC.IDPROCESO = ACCESO.IDPROCESO AND IDINSTITUCION = '"+ request.getIdInstitucion() + "'   and acceso.idperfil = '" + request.getIdGrupo() + "')");
 		// || !request.getIdInstitucionCertificado().equals(SigaConstants.InstitucionGeneral)
-		  if (!request.getIdInstitucion().equals(SigaConstants.InstitucionGeneral)) {
+		  if (!idInstitucionCert.equals(SigaConstants.InstitucionGeneral)) {
 	        	sql.WHERE(" UPPER(PROC.DESCRIPCION) not like UPPER('%hidden%')");
 			}
 		sql.ORDER_BY("PARENT DESC, TEXT ASC");
--- siga-core/src/main/resources/bd/CENSO II/2019-03-29/1 - Traducciones.sql
+++ siga-core/src/main/resources/bd/CENSO II/2019-03-29/1 - Traducciones.sql
@@ -1,4 +1,52 @@
-UPDATE gen_diccionario SET descripcion = 'Colegiaciones en otros colegios:' WHERE idrecurso = 'censo.datosDireccion.literal.numero.colegiaciones' and idlenguaje = 1;
+UPDATE gen_diccionario SET descripcion = 'Colegiaciones en otros colegios:'    WHERE idrecurso = 'censo.datosDireccion.literal.numero.colegiaciones' and idlenguaje = 1;
 UPDATE gen_diccionario SET descripcion = 'Col·legiacions en altres col·legis:' WHERE idrecurso = 'censo.datosDireccion.literal.numero.colegiaciones' and idlenguaje = 2;
 UPDATE gen_diccionario SET descripcion = 'Colegiaciones en otros colegios:#EU' WHERE idrecurso = 'censo.datosDireccion.literal.numero.colegiaciones' and idlenguaje = 3;
 UPDATE gen_diccionario SET descripcion = 'Colegiaciones en otros colegios:#GL' WHERE idrecurso = 'censo.datosDireccion.literal.numero.colegiaciones' and idlenguaje = 4;
+
+
+
+ALTER TABLE FOR_PERSONA_CURSO
+DROP CONSTRAINT FOR_PER_CURSO_FOR_ROLES_FK;
+
+ALTER TABLE for_roles
+DROP CONSTRAINT FOR_ROLES_PK;
+
+
+ALTER TABLE for_roles
+ADD CONSTRAINT FOR_ROLES_PK PRIMARY KEY (IDROL,IDINSTITUCION);
+
+ALTER TABLE for_persona_curso
+    ADD CONSTRAINT for_per_curso_for_roles_fk FOREIGN KEY ( idrol,IDINSTITUCION )
+        REFERENCES for_roles ( idrol,IDINSTITUCION );
+
+		
+		ALTER TABLE FOR_TEMACURSO_CURSO
+DROP CONSTRAINT FOR_TEMACU_CURSO_FOR_TEM_FK;
+
+ALTER TABLE FOR_TEMACURSO_PERSONA
+DROP CONSTRAINT FOR_TEMC_PERS_FOR_CURSO_FK;
+
+ALTER TABLE FOR_TEMACURSO
+DROP CONSTRAINT FOR_TEMACURSO_PK;
+
+
+ALTER TABLE FOR_TEMACURSO
+ADD CONSTRAINT FOR_TEMACURSO_PK PRIMARY KEY (IDTEMACURSO,IDINSTITUCION);
+
+ALTER TABLE FOR_TEMACURSO_CURSO
+    ADD CONSTRAINT FOR_TEMACU_CURSO_FOR_TEM_FK FOREIGN KEY ( IDTEMACURSO,IDINSTITUCION )
+        REFERENCES FOR_TEMACURSO ( IDTEMACURSO,IDINSTITUCION );
+
+ALTER TABLE FOR_TEMACURSO_PERSONA
+    ADD CONSTRAINT FOR_TEMC_PERS_FOR_CURSO_FK FOREIGN KEY ( IDTEMACURSO,IDINSTITUCION )
+        REFERENCES FOR_TEMACURSO ( IDTEMACURSO,IDINSTITUCION );
+		
+		
+		
+		
+		
+Insert into GEN_CATALOGOS_MULTIIDIOMA (CODIGO,NOMBRETABLA,CAMPOTABLA,FECHAMODIFICACION,USUMODIFICACION,LOCAL,CODIGOTABLA,MIGRADO) 
+values ((select max(codigo) +1 from GEN_CATALOGOS_MULTIIDIOMA),'FOR_TEMACURSO','DESCRIPCION',sysdate,'0','N','IDTEMACURSO','S');
+Insert into GEN_CATALOGOS_MULTIIDIOMA (CODIGO,NOMBRETABLA,CAMPOTABLA,FECHAMODIFICACION,USUMODIFICACION,LOCAL,CODIGOTABLA,MIGRADO) 
+values ((select max(codigo) +1 from GEN_CATALOGOS_MULTIIDIOMA),'FOR_ROLES','DESCRIPCION',sysdate,'0','N','IDROL','S');
+		
