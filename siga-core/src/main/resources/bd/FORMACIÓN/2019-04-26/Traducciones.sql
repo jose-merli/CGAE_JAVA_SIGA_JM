0fc64e3f58d59263b8aeaa3dd2c6515890a7a133
Fredi Jerbi <fjerbi@deloitte.es>
2019-04-22 12:18:38 +0200

    [fjerbi] Traducciones para Informes y comunicaciones

---
+++ siga-core/src/main/resources/bd/FORMACIÓN/2019-04-26/Traducciones.sql
@@ -0,0 +1,4 @@
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('informesycomunicaciones.plantillasenvio.ficha.remitente','Remitente','0','1',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('informesycomunicaciones.plantillasenvio.ficha.remitente','Remitent','0','2',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('informesycomunicaciones.plantillasenvio.ficha.remitente','Remitente#EU','0','3',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('informesycomunicaciones.plantillasenvio.ficha.remitente','Remitente#GL','0','4',to_date('04/02/19','DD/MM/RR'),'0','19');
