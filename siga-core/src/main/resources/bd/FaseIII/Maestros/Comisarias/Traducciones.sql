e617e98a26da23052892233086ac69c26bc469c2
Iván Partida <ipartida@deloitte.es>
2019-10-18 13:21:18 +0200

    [ipartida] Añadida traducciones varias pantallas

---
+++ siga-core/src/main/resources/bd/FaseIII/Maestros/Comisarias/Traducciones.sql
@@ -0,0 +1,5 @@
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.facturacionSJCS.mantenimientoGestionComisarias','Gestión Comisarías','0','1',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.facturacionSJCS.mantenimientoGestionComisarias','Gestió Comissaries','0','2',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.facturacionSJCS.mantenimientoGestionComisarias','Gestión Comisarías#EU','0','3',to_date('04/02/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.facturacionSJCS.mantenimientoGestionComisarias','Gestión Comisarías#GL','0','4',to_date('04/02/19','DD/MM/RR'),'0','19');
+
