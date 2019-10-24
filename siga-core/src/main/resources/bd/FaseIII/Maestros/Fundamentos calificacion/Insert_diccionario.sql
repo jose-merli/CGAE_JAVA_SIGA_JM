fd533e8b6809cd8c0c3e42beb00591055f669000
Pablo Kammerer <pkammerer@deloitte.es>
2019-10-24 15:20:45 +0200

    [pkammerer] => diccionarios y scripts

---
+++ siga-core/src/main/resources/bd/FaseIII/Maestros/Fundamentos calificacion/Insert_diccionario.sql
@@ -0,0 +1,23 @@
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.tabla.sinResultados','Sin resultados','0','1',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.tabla.sinResultados','Sense resultats','0','2',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.tabla.sinResultados','Sin resultados#GL','0','4',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.tabla.sinResultados','Sin resultados#EU','0','3',to_date('24/09/19','DD/MM/RR'),'0','19');
+
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.fundamentos.fundamentosCalificacion','Fundamentos Calificación','0','1',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.fundamentos.fundamentosCalificacion','Fonaments Qualificació','0','2',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.fundamentos.fundamentosCalificacion','Fundamentos Calificación#GL','0','4',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.fundamentos.fundamentosCalificacion','Fundamentos Calificación#EU','0','3',to_date('24/09/19','DD/MM/RR'),'0','19');
+
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.gestion.gestionFundamentosCalificacion','Gestión Fundamentos Calificación','0','1',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.gestion.gestionFundamentosCalificacion','Gestió Fonaments Qualificació','0','2',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.gestion.gestionFundamentosCalificacion','Gestión Fundamentos Calificación#GL','0','4',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.gestion.gestionFundamentosCalificacion','Gestión Fundamentos Calificación#EU','0','3',to_date('24/09/19','DD/MM/RR'),'0','19');
+
+
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.datosGenerales.dictamen','Dictamen','0','1',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.datosGenerales.dictamen','Dictamen','0','2',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.datosGenerales.dictamen','Dictamen#GL','0','4',to_date('24/09/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('justiciaGratuita.maestros.fundamentosCalificacion.datosGenerales.dictamen','Dictamen#EU','0','3',to_date('24/09/19','DD/MM/RR'),'0','19');
