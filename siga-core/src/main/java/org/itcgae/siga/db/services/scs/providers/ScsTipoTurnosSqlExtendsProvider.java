aca536be2c2ce3a4aa2c2e4699e30efa8db794f2
Iván Partida <ipartida@deloitte.es>
2019-10-28 13:57:31 +0100

    [ipartida] Cambios combos

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsTipoTurnosSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsTipoTurnosSqlExtendsProvider.java
@@ -1,19 +1,20 @@
 package org.itcgae.siga.db.services.scs.providers;
 
 import org.apache.ibatis.jdbc.SQL;
+import org.itcgae.siga.db.entities.AdmUsuarios;
 import org.itcgae.siga.db.mappers.ScsTipoactuacionSqlProvider;
 import org.itcgae.siga.db.mappers.ScsTurnoSqlProvider;
 
 public class ScsTipoTurnosSqlExtendsProvider extends ScsTurnoSqlProvider {
 
-	public String comboTipoTurnos(Short idInstitucion) {
+	public String comboTipoTurnos(String idLenguaje) {
 
 		SQL sql = new SQL();
 
-		sql.SELECT("IDTURNO, NOMBRE");
-		sql.FROM("SCS_TURNO");
-		sql.WHERE("IDINSTITUCION = '"+idInstitucion +"'");
-		sql.WHERE("FECHABAJA IS NULL");
+		sql.SELECT("SCS_TIPOTURNO.IDTIPOTURNO, GEN_RECURSOS_CATALOGOS.DESCRIPCION");
+		sql.FROM("SCS_TIPOTURNO");
+		sql.LEFT_OUTER_JOIN("GEN_RECURSOS_CATALOGOS ON SCS_TIPOTURNO.DESCRIPCION = GEN_RECURSOS_CATALOGOS.IDRECURSO");
+		sql.WHERE("SCS_TIPOTURNO.FECHA_BAJA IS NULL AND GEN_RECURSOS_CATALOGOS.IDLENGUAJE ='"+idLenguaje+"'");
 		return sql.toString();
 	}
 
