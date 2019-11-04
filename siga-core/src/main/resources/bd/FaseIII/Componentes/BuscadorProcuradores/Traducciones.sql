7997f25bc19577eac145dd68cff5c818243c46b0
Pablo Kammerer <pkammerer@deloitte.es>
2019-11-04 16:55:17 +0100

    [pkammerer] => traduccion BuscadorProcuradores

---
+++ siga-core/src/main/resources/bd/FaseIII/Componentes/BuscadorProcuradores/Traducciones.sql
@@ -0,0 +1,4 @@
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.justiciaGratuita.componentes.estadoColegial','Estado Colegial','0','1',to_date('04/11/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.justiciaGratuita.componentes.estadoColegial','Estat Col·legial','0','2',to_date('04/11/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.justiciaGratuita.componentes.estadoColegial','Estado Colegial#EU','0','3',to_date('04/11/19','DD/MM/RR'),'0','19');
+Insert into GEN_DICCIONARIO (IDRECURSO,DESCRIPCION,ERROR,IDLENGUAJE,FECHAMODIFICACION,USUMODIFICACION,IDPROPIEDAD) values ('menu.justiciaGratuita.componentes.estadoColegial','Estado Colegial#GL','0','4',to_date('04/11/19','DD/MM/RR'),'0','19');
