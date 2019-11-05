b625a0fff5e88b45008f90d6e08754a01a008626
Pablo Kämmerer <pkammerer@deloitte.es>
2019-11-05 15:36:47 +0100

    [pkammerer] => Puliendo BuscadorProcuradores

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsProcuradorSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsProcuradorSqlExtendsProvider.java
@@ -75,8 +75,8 @@ public class ScsProcuradorSqlExtendsProvider extends ScsProcuradorSqlProvider {
 
 		SQL sql = new SQL();
 
-		sql.SELECT("apellidos1");
-		sql.SELECT("apellidos2");
+		sql.SELECT("TRIM(apellidos1) as apellidos1");
+		sql.SELECT("TRIM(apellidos2) as apellidos2");
 		sql.SELECT("codigo");
 		sql.SELECT("codigopostal");
 		sql.SELECT("domicilio");
@@ -87,23 +87,23 @@ public class ScsProcuradorSqlExtendsProvider extends ScsProcuradorSqlProvider {
 		sql.SELECT("idcolprocurador");
 		sql.SELECT("idpoblacion");
 		sql.SELECT("idprovincia");
-		sql.SELECT_DISTINCT("concat(apellidos1 || ' ', apellidos2 || '') AS nombreape");
+		sql.SELECT_DISTINCT("TRIM(concat(concat(apellidos1 || '', ' '), apellidos2 || '')) AS nombreape");
 		sql.SELECT("ncolegiado");
-		sql.SELECT("pro.nombre");
+		sql.SELECT("TRIM(pro.nombre) as nombre");
 		sql.SELECT("telefono1");
 		sql.SELECT("telefono2");
 		sql.SELECT("abreviatura");
 		sql.SELECT("idprocurador");
 
 		sql.FROM("scs_procurador PRO");
-		
+
 		sql.LEFT_OUTER_JOIN("CEN_INSTITUCION INST ON PRO.IDINSTITUCION = INST.IDINSTITUCION");
-		
-		if(procuradorItem.getIdInstitucion() != null && !procuradorItem.getIdInstitucion().trim().equals("")) 
-			sql.WHERE("pro.idinstitucion in ("+procuradorItem.getIdInstitucion()+")");
-		
-		else	
-		sql.WHERE("pro.idinstitucion = '" + idInstitucion + "'");
+
+		if (procuradorItem.getIdInstitucion() != null && !procuradorItem.getIdInstitucion().trim().equals(""))
+			sql.WHERE("pro.idinstitucion in (" + procuradorItem.getIdInstitucion() + ")");
+		else if (idInstitucion.equals("2000") && procuradorItem.getIdInstitucion() == null || procuradorItem.getIdInstitucion().equals("")) {
+		} else
+			sql.WHERE("pro.idinstitucion = '" + idInstitucion + "'");
 
 		if (procuradorItem.getnColegiado() != null && !procuradorItem.getnColegiado().trim().equals(""))
 			sql.WHERE("ncolegiado = '" + procuradorItem.getnColegiado() + "'");
@@ -111,13 +111,11 @@ public class ScsProcuradorSqlExtendsProvider extends ScsProcuradorSqlProvider {
 		if (procuradorItem.getApellido1() != null && !procuradorItem.getApellido1().trim().equals(""))
 			sql.WHERE("TRANSLATE(LOWER(apellidos1),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')  LIKE TRANSLATE(LOWER('%"
 					+ procuradorItem.getApellido1() + "%'),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')");
-		if (procuradorItem.getApellido2() != null && !procuradorItem.getApellido2().trim().equals("")
-				&& procuradorItem.getApellido2().trim().length() > 2)
+		if (procuradorItem.getApellido2() != null && !procuradorItem.getApellido2().trim().equals(""))
 			sql.WHERE("TRANSLATE(LOWER(apellidos2),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')  LIKE TRANSLATE(LOWER('%"
 					+ procuradorItem.getApellido2() + "%'),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')");
 
-		if (procuradorItem.getNombre() != null && !procuradorItem.getNombre().trim().equals("")
-				&& procuradorItem.getNombre().trim().length() > 2)
+		if (procuradorItem.getNombre() != null && !procuradorItem.getNombre().trim().equals(""))
 			sql.WHERE("TRANSLATE(LOWER(pro.NOMBRE),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')  LIKE TRANSLATE(LOWER('%"
 					+ procuradorItem.getNombre() + "%'),'áéíóúüñÁÉÍÓÚÜÑ','aeiouunAEIOUUN')");
 
