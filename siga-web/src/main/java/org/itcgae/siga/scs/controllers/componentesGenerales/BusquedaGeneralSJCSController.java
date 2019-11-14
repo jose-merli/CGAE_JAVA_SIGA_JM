ade606d3eecbc17aede13b08fe0cb6e657d4bb3d
Fredi Jerbi <fjerbi@deloitte.es>
2019-11-14 09:22:56 +0100

    [fjerbi] Agregada pantalla Busqueda General SJCS

---
+++ siga-web/src/main/java/org/itcgae/siga/scs/controllers/componentesGenerales/BusquedaGeneralSJCSController.java
@@ -0,0 +1,33 @@
+package org.itcgae.siga.scs.controllers.componentesGenerales;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.itcgae.siga.DTO.scs.ColegiadosSJCSDTO;
+import org.itcgae.siga.DTO.scs.ColegiadosSJCSItem;
+import org.itcgae.siga.DTOs.gen.ComboDTO;
+import org.itcgae.siga.scs.services.componentesGenerales.BusquedaGeneralSJCSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/componenteGeneralJG")
+public class BusquedaGeneralSJCSController {
+	
+	
+	@Autowired
+	private BusquedaGeneralSJCSService busquedaGeneralSJCSService;
+	
+
+	@PostMapping("/busquedaGeneralSJCS")
+	public ResponseEntity<ColegiadosSJCSDTO> searchGeneralSJCS(HttpServletRequest request, @RequestBody ColegiadosSJCSItem colegiadosSJCSItem) {
+		ColegiadosSJCSDTO response = busquedaGeneralSJCSService.searchColegiadosSJCS(request, colegiadosSJCSItem);
+		return new ResponseEntity<ColegiadosSJCSDTO>(response, HttpStatus.OK);
+	}
+	
+}
