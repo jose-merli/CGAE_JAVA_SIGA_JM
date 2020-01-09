dab39ed593016a4e863f5e1a5df4078f052ccc9b
Iván Partida <ipartida@deloitte.es>
2020-01-09 11:21:17 +0100

    [prodriguezmunoz] Arreglos Fetch en nueva máquina

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsTipoEJGColegioSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsTipoEJGColegioSqlExtendsProvider.java
@@ -1,13 +1,12 @@
 package org.itcgae.siga.db.services.scs.providers;
 
 import org.apache.ibatis.jdbc.SQL;
-import org.itcgae.siga.db.mappers.ScsTipoejgSqlProvider;
 import org.itcgae.siga.db.mappers.ScsTipoejgcolegioSqlProvider;
 
 public class ScsTipoEJGColegioSqlExtendsProvider extends ScsTipoejgcolegioSqlProvider {
 
 	public String comboTipoEjgColegio(Short idLenguaje) {
-
+ 
 		SQL sql = new SQL();
 
 		sql.SELECT("tipoejg.IDTIPOEJGCOLEGIO");
@@ -21,5 +20,19 @@ public class ScsTipoEJGColegioSqlExtendsProvider extends ScsTipoejgcolegioSqlPro
 
 		return sql.toString();
 	}
+    public String comboTipoColegioEjg(String idLenguaje, String idInstitucion) {
+        SQL sql = new SQL();
+        
+        sql.SELECT("tipocolegioejg.IDTIPOEJGCOLEGIO");
+        sql.SELECT("catalogoTipocolegioejg.DESCRIPCION");
+
+        sql.FROM("SCS_TIPOEJGCOLEGIO tipocolegioejg");
+        sql.LEFT_OUTER_JOIN("GEN_RECURSOS_CATALOGOS catalogoTipocolegioejg on catalogoTipocolegioejg.idrecurso = tipocolegioejg.DESCRIPCION and catalogoTipocolegioejg.idlenguaje ="+idLenguaje);
+        sql.WHERE("tipocolegioejg.fecha_baja is null and tipocolegioejg.idinstitucion =" + idInstitucion);
+        sql.ORDER_BY("descripcion");
+
+        return sql.toString();
+    }
+
 	
 }
