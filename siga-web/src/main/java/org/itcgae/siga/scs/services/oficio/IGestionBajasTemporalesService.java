cf282e5979a31ac6e31d83e137370c79982be940
Jose Antonio Ruiz <jruizdona@deloitte.es>
2021-03-09 17:28:33 +0100

    [jruizdona] Busqueda Bajas Temporales

--- siga-web/src/main/java/org/itcgae/siga/scs/services/oficio/IGestionBajasTemporalesService.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/oficio/IGestionBajasTemporalesService.java
@@ -23,4 +23,10 @@ public interface IGestionBajasTemporalesService {
 
 	public BajasTemporalesDTO busquedaBajasTemporales(BajasTemporalesItem bajasTemporalesItem, HttpServletRequest request);
 	
+	public UpdateResponseDTO updateAnular(BajasTemporalesDTO bajasTemporalesDTO, HttpServletRequest request);
+
+	public UpdateResponseDTO updateValidar(BajasTemporalesDTO bajasTemporalesDTO, HttpServletRequest request);
+	
+	public UpdateResponseDTO updateDenegar(BajasTemporalesDTO bajasTemporalesDTO, HttpServletRequest request);
+	
 }
