80616a63b3a7efe8dd72a68ef4aede76480420f3
Rubén Laborda <rlaborda@deloitte.es>
2021-03-23 16:28:33 +0100

    Eliminar wsdl y .bat innecesario para servicio de sociedades

--- siga-core/src/main/resources/xsd/registroSociedades/v1/WSDL2JavaServer.bat
+++
@@ -1,7 +0,0 @@
-set URL_WSDL=./RegistroSociedadesServer.wsdl
-
-set PAQUETE=org.itcgae.sspp.ws.registroSociedades
-set AXIS2_HOME=C:\Program Files\axis2-1.6.2
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
-
-"%AXIS2_HOME%\bin\WSDL2Java" -uri %URL_WSDL% -p %PAQUETE% -d xmlbeans -S java -o tmp/registroSociedades -sp -ssi -ss -sd -g -ns2p http://www.w3.org/2004/06/xmlmime=%PAQUETE%.wsdl.xmlmime,http://schemas.xmlsoap.org/soap/encoding/=%PAQUETE%.wsdl.soap.encoding
