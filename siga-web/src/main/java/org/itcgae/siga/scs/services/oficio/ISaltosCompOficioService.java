23ab55df57514f6c0388ff45d5f8856035e88644
Miguel Ángel Daza Laprida <mdaza@deloitte.es>
2021-04-01 12:27:45 +0200

    Controller para saltos y compensaciones de oficio, se ha implementado los métodos de búsqueda, anulación y borrado

--- siga-web/src/main/java/org/itcgae/siga/scs/services/oficio/ISaltosCompOficioService.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/oficio/ISaltosCompOficioService.java
@@ -1,4 +1,4 @@
-package org.itcgae.siga.scs.services.guardia;
+package org.itcgae.siga.scs.services.oficio;
 
 import java.util.List;
 
@@ -8,17 +8,14 @@ import org.itcgae.siga.DTOs.adm.DeleteResponseDTO;
 import org.itcgae.siga.DTOs.scs.SaltoCompGuardiaDTO;
 import org.itcgae.siga.DTOs.scs.SaltoCompGuardiaItem;
 
-public interface SaltosCompGuardiasService {
+public interface ISaltosCompOficioService {
 
 	public SaltoCompGuardiaDTO searchSaltosYCompensaciones(SaltoCompGuardiaItem saltoItem, HttpServletRequest request);
 
-	public DeleteResponseDTO guardarSaltosCompensaciones(List<SaltoCompGuardiaItem> listaSaltoItem,
+	public DeleteResponseDTO anularSaltosCompensaciones(List<SaltoCompGuardiaItem> listaSaltoItem,
 			HttpServletRequest request);
 
 	public DeleteResponseDTO borrarSaltosCompensaciones(List<SaltoCompGuardiaItem> listaSaltoItem,
-			HttpServletRequest request);
-
-	public DeleteResponseDTO anularSaltosCompensaciones(List<SaltoCompGuardiaItem> listaSaltoItem,
 			HttpServletRequest request);
 
 }
