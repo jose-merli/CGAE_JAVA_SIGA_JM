8ad6341f046a11c6798742e60b3bf917d574622a
Jose Luis Romero <jromeroprieto@deloitte.es>
2021-04-26 14:25:20 +0200

    [jromeroprieto] eliminar relacion se cambia al mapper

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/oficio/DesignacionesServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/oficio/DesignacionesServiceImpl.java
@@ -123,6 +123,7 @@ public class DesignacionesServiceImpl implements IDesignacionesService {
 	
 	@Autowired
 	private CenColegiadoMapper cenColegiadoMapper;
+	
 	/**
 	 * busquedaJustificacionExpres
 	 */
@@ -3081,7 +3082,9 @@ public class DesignacionesServiceImpl implements IDesignacionesService {
 					LOGGER.info(
 							"deleteInteresado() / ScsDefendidosdesignaMapper.deleteByPrimaryKey() -> Entrada a ScsDefendidosdesignaMapper para eliminar los contrarios seleccionados");
 
-					response = scsDesignacionesExtendsMapper.eliminarRelacion(dni);
+					ScsDesignaKey key = new ScsDesignaKey();
+					
+					response = scsDesignaMapper.deleteByPrimaryKey(key);
 
 					LOGGER.info(
 							"deleteInteresado() / ScsDefendidosdesignaMapper.deleteByPrimaryKey() -> Salida de ScsDefendidosdesignaMapper para eliminar los contrarios seleccionados");
