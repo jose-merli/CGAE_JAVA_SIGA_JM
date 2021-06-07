93964fdc43b93db76707ebbe943ba15f74a5768d
DESKTOP-A4G0MED\aavalosmoreno <aavalosmoreno@deloitte.es>
2021-06-07 16:50:49 +0200

    [aavalosmoreno] (Oficio - Designaciones - FIcha Designaciones) Arreglo a --> Se muestran los datos del letrado inicial de la Designa y No del sustituto.

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsDesignacionesSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsDesignacionesSqlExtendsProvider.java
@@ -2857,7 +2857,7 @@ public class ScsDesignacionesSqlExtendsProvider extends ScsDesignaSqlProvider {
 		if(designa.getNumprocedimiento() != null) {
 			sql.WHERE("SCS_DESIGNASLETRADO.IDPERSONA = '" + designa.getNumprocedimiento() + "'");
 		}
-		sql.ORDER_BY("SCS_DESIGNASLETRADO.FECHADESIGNA DESC, SCS_DESIGNASLETRADO.LETRADODELTURNO DESC");
+		sql.ORDER_BY("scs_designasletrado.fechadesigna DESC, scs_designasletrado.fecharenunciasolicita DESC NULLS FIRST, SCS_DESIGNASLETRADO.LETRADODELTURNO DESC");
 
 		return sql.toString();
 	}
