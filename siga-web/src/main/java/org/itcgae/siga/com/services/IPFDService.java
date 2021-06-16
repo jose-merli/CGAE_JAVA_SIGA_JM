255930fcd3efb159f891e18508f51cc0c6a9b738
Jose Luis Romero <jromeroprieto@deloitte.es>
2021-06-16 16:24:09 +0200

    [jromeroprieto] servicio de descargar de eejg

--- siga-web/src/main/java/org/itcgae/siga/com/services/IPFDService.java
+++ siga-web/src/main/java/org/itcgae/siga/com/services/IPFDService.java
@@ -4,11 +4,9 @@ import java.io.File;
 
 public interface IPFDService {
 	
-	public String firmarPDF(File fichero) throws Exception;
-
 	public String obtenerDocumentoFirmado(String csv) throws Exception;	
 
 	public byte[] getBytes(String file) throws Exception;
 	
-	public File createFile(byte[] bytes,String path,String fileName) throws Exception;
+	public File createTempFile(byte[] bytes,String fileName) throws Exception;
 }
