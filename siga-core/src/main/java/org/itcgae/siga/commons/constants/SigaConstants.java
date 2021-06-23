858dd0f334b70e007d8b62437e060e6b5f2d37f0
Fernando Gallego <fgallego@deloitte.es>
2021-06-23 15:11:55 +0200

    Subida de version

--- siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
+++ siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
@@ -1024,6 +1024,6 @@ public static final String ESTADO_CURSO_ABIERTO = "0";
 	public static int ECOM_COLA_HORAS_EN_EJECUCION_MAXIMAS = 2;
 	
 	public static String SIGAFRONT_VERSION = "202106251500";
-	public static String SIGAWEB_VERSION = "1.0.71_4";
+	public static String SIGAWEB_VERSION = "1.0.71_5";
 	
 }
