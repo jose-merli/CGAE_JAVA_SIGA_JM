0599b837c943b873695e7d24501dfe4f80eb7d01
Guillermo Rejon <grejon@deloitte.es>
2021-09-09 14:59:28 +0200

    Creacion de servicio de getEJG.

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/oficio/DesignacionesServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/oficio/DesignacionesServiceImpl.java
@@ -131,6 +131,7 @@ import org.itcgae.siga.db.entities.ScsDocumentaciondesigna;
 import org.itcgae.siga.db.entities.ScsDocumentaciondesignaExample;
 import org.itcgae.siga.db.entities.ScsDocumentaciondesignaKey;
 import org.itcgae.siga.db.entities.ScsEjg;
+import org.itcgae.siga.db.entities.ScsEjgExample;
 import org.itcgae.siga.db.entities.ScsEjgKey;
 import org.itcgae.siga.db.entities.ScsEjgWithBLOBs;
 import org.itcgae.siga.db.entities.ScsEjgdesigna;
@@ -939,6 +940,9 @@ public class DesignacionesServiceImpl implements IDesignacionesService {
 
 		return designas;
 	}
+
+
+	
 
 	@Override
 	public List<DesignaItem> busquedaModuloDesignas(DesignaItem designaItem, HttpServletRequest request) {
@@ -7507,6 +7511,51 @@ public class DesignacionesServiceImpl implements IDesignacionesService {
 		}
 
 		return responseDTO;
+	}
+
+
+	@Override
+	public ScsEjg getEJG(EjgItem item, HttpServletRequest request) {
+		
+		String token = request.getHeader("Authorization");
+		String dni = UserTokenUtils.getDniFromJWTToken(token);
+		Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
+		Error error = new Error();
+		int response = 0;
+		
+		ScsEjg ejg = new ScsEjg();
+
+		if (idInstitucion != null) {
+			AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
+			exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
+
+			LOGGER.info(
+					"DesignacionesServiceImpl.getEJG() -> Entrada a admUsuariosExtendsMapper para obtener información del usuario logeado");
+
+			List<AdmUsuarios> usuarios = admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
+
+			LOGGER.info(
+					"DesignacionesServiceImpl.getEJG() -> Salida de admUsuariosExtendsMapper para obtener información del usuario logeado");
+
+			if (usuarios != null && usuarios.size() > 0) {
+				LOGGER.info(
+						"DesignacionesServiceImpl.getEJG() -> Entrada a servicio para insertar laobtener la infromacion de un EJG");
+
+					LOGGER.info("DesignacionesServiceImpl.getEJG() -> Haciendo la consulta...");
+
+					ScsEjgExample ejgExample = new ScsEjgExample();
+					
+					ejgExample.createCriteria().andAnioEqualTo(Short.valueOf(item.getAnnio())).andIdinstitucionactaEqualTo(idInstitucion)
+					.andNumejgEqualTo(item.getNumEjg());
+					List<ScsEjg> ejgs = scsEjgMapper.selectByExample(ejgExample);
+					
+					ejg = ejgs.get(0);
+			}
+		}
+		
+		LOGGER.info("DesignacionesServiceImpl.getEJG() -> Saliendo del servicio...");
+
+		return ejg;
 	}
 
 }
