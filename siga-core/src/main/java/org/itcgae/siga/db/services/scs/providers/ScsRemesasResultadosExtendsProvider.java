ea23ca8aacc6124caf1a21da3bb9a6c8fbd7a0b7
Rubén Laborda <rlaborda@deloitte.es>
2021-09-20 10:58:02 +0200

    Añadir filtro a la búsqueda

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsRemesasResultadosExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsRemesasResultadosExtendsProvider.java
@@ -11,9 +11,9 @@ public class ScsRemesasResultadosExtendsProvider {
 	
 	private Logger LOGGER = Logger.getLogger(ScsRemesasResultadosExtendsProvider.class);
 	
-	public String  buscarRemesasResultado(RemesasResultadoItem remesasBusquedaItem, Short idInstitucion) {
+	public String  buscarRemesasResultado(RemesasResultadoItem remesasResultadoItem, Short idInstitucion) {
 		SQL sql = new SQL();
-		//DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		
 		sql.SELECT("REMR.IDREMESARESOLUCION");
 		sql.SELECT("REMR.PREFIJO AS NUMREMESAPREFIJO");
@@ -32,8 +32,62 @@ public class ScsRemesasResultadosExtendsProvider {
 		sql.FROM("CAJG_REMESARESOLUCION REMR");
 		sql.LEFT_OUTER_JOIN("CAJG_REMESA REM ON REM.IDINSTITUCION = REMR.IDINSTITUCION");
 		
+		if(remesasResultadoItem.getNumRemesaPrefijo() != null) {
+			sql.WHERE("REM.PREFIJO = '" + remesasResultadoItem.getNumRemesaPrefijo() + "'");
+		}
+		
+		if(remesasResultadoItem.getNumRemesaNumero() != null) {
+			sql.WHERE("REM.NUMERO = '" + remesasResultadoItem.getNumRemesaNumero() + "'");
+		}
+		
+		if(remesasResultadoItem.getNumRemesaSufijo() != null) {
+			sql.WHERE("REM.SUFIJO = '" + remesasResultadoItem.getNumRemesaSufijo() + "'");
+		}
+		
+		if(remesasResultadoItem.getNumRegistroPrefijo() != null) {
+			sql.WHERE("REMR.PREFIJO = '" + remesasResultadoItem.getNumRegistroPrefijo() + "'");
+		}
+		
+		if(remesasResultadoItem.getNumRegistroNumero() != null) {
+			sql.WHERE("REMR.NUMERO = '" + remesasResultadoItem.getNumRegistroNumero() + "'");
+		}
+		
+		if(remesasResultadoItem.getNumRegistroSufijo() != null) {
+			sql.WHERE("REMR.SUFIJO = '" + remesasResultadoItem.getNumRegistroSufijo() + "'");
+		}
+		
+		if(remesasResultadoItem.getNombreFichero() != null) {
+			sql.WHERE("REMR.NOMBREFICHERO = '" + remesasResultadoItem.getNombreFichero() + "'");
+		}
+		
+		if(remesasResultadoItem.getFechaRemesaDesde() != null) {
+			String fechaRemesaDesde = "";
+			fechaRemesaDesde = dateFormat.format(remesasResultadoItem.getFechaRemesaDesde());
+			sql.WHERE("TRUNC(REMR.FECHARESOLUCION) >= TO_DATE('" + fechaRemesaDesde + "', 'DD/MM/RRRR')");
+		}
+		
+		if(remesasResultadoItem.getFechaRemesaHasta() != null) {
+			String fechaRemesaHasta = "";
+			fechaRemesaHasta = dateFormat.format(remesasResultadoItem.getFechaRemesaHasta());
+			sql.WHERE("TRUNC(REMR.FECHARESOLUCION) <= TO_DATE('" + fechaRemesaHasta + "', 'DD/MM/RRRR')");
+		}
+		
+		if(remesasResultadoItem.getFechaCargaDesde() != null) {
+			String fechaCargaDesde = "";
+			fechaCargaDesde = dateFormat.format(remesasResultadoItem.getFechaCargaDesde());
+			sql.WHERE("TRUNC(REMR.FECHACARGA) >= TO_DATE('" + fechaCargaDesde + "', 'DD/MM/RRRR')");
+		}
+		
+		if(remesasResultadoItem.getFechaCargaHasta() != null) {
+			String fechaCargaHasta = "";
+			fechaCargaHasta = dateFormat.format(remesasResultadoItem.getFechaCargaHasta());
+			sql.WHERE("TRUNC(REMR.FECHACARGA) <= TO_DATE('" + fechaCargaHasta + "', 'DD/MM/RRRR')");
+		}
+		
 		sql.WHERE("REMR.IDINSTITUCION = " + idInstitucion);
 		sql.WHERE("REMR.IDTIPOREMESA = 3");
+		sql.WHERE("REM.IDINSTITUCION = REMR.IDINSTITUCION");
+		sql.WHERE("REM.IDREMESA = REMR.IDREMESA");
 		sql.WHERE("ROWNUM <= 200");
 		
 		sql.ORDER_BY("REMR.IDREMESARESOLUCION DESC");
