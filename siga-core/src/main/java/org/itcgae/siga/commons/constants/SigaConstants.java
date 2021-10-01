dd53709bcca8c8764e667a131064a3bc03527dd4
Jesús Rivero <jriveromuniz@deloitte.es>
2021-10-01 12:40:34 +0200

    Modificado el número de versión

--- siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
+++ siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
@@ -1034,6 +1034,6 @@ public static final String ESTADO_CURSO_ABIERTO = "0";
 	public static int ECOM_COLA_HORAS_EN_EJECUCION_MAXIMAS = 2;
 	
 	public static String SIGAFRONT_VERSION = "202110011300";
-	public static String SIGAWEB_VERSION = "1.0.74_1";
+	public static String SIGAWEB_VERSION = "1.0.71_8";
 	
 }
