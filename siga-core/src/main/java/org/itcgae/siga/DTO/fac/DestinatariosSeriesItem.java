d8897a03f15144c2715ea49d9ce00f266de33fc6
Antonio Suárez Bono <asuarezbono@deloitte.es>
2021-10-27 17:46:07 +0200

    Mostrar destinatarios en ficha de serie de facturación

--- siga-core/src/main/java/org/itcgae/siga/DTO/fac/DestinatariosSeriesItem.java
+++ siga-core/src/main/java/org/itcgae/siga/DTO/fac/DestinatariosSeriesItem.java
@@ -6,6 +6,8 @@ public class DestinatariosSeriesItem {
 	
 	private String idPersona;
 	private String nombre;
+	private String apellidos1;
+	private String apellidos2;
 	private String nifCif;
 	private String movil;
 	private String correoElectronico;
@@ -25,6 +27,22 @@ public class DestinatariosSeriesItem {
 	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	
+	public String getApellidos1() {
+		return apellidos1;
+	}
+	
+	public void setApellidos1(String apellidos1) {
+		this.apellidos1 = apellidos1;
+	}
+	
+	public String getApellidos2() {
+		return apellidos2;
+	}
+	
+	public void setApellidos2(String apellidos2) {
+		this.apellidos2 = apellidos2;
 	}
 	
 	public String getNifCif() {
@@ -61,7 +79,7 @@ public class DestinatariosSeriesItem {
 	
 	@Override
 	public int hashCode() {
-		return Objects.hash(correoElectronico, domicilio, idPersona, movil, nifCif, nombre);
+		return Objects.hash(apellidos1, apellidos2, correoElectronico, domicilio, idPersona, movil, nifCif, nombre);
 	}
 	
 	@Override
@@ -73,15 +91,19 @@ public class DestinatariosSeriesItem {
 		if (getClass() != obj.getClass())
 			return false;
 		DestinatariosSeriesItem other = (DestinatariosSeriesItem) obj;
-		return Objects.equals(correoElectronico, other.correoElectronico) && Objects.equals(domicilio, other.domicilio)
-				&& Objects.equals(idPersona, other.idPersona) && Objects.equals(movil, other.movil)
-				&& Objects.equals(nifCif, other.nifCif) && Objects.equals(nombre, other.nombre);
+		return Objects.equals(apellidos1, other.apellidos1) && Objects.equals(apellidos2, other.apellidos2)
+				&& Objects.equals(correoElectronico, other.correoElectronico)
+				&& Objects.equals(domicilio, other.domicilio) && Objects.equals(idPersona, other.idPersona)
+				&& Objects.equals(movil, other.movil) && Objects.equals(nifCif, other.nifCif)
+				&& Objects.equals(nombre, other.nombre);
 	}
 	
 	@Override
 	public String toString() {
-		return "DestinatariosSeriesItem [idPersona=" + idPersona + ", nombre=" + nombre + ", nifCif=" + nifCif
-				+ ", movil=" + movil + ", correoElectronico=" + correoElectronico + ", domicilio=" + domicilio + "]";
+		return "DestinatariosSeriesItem [idPersona=" + idPersona + ", nombre=" + nombre + ", apellidos1=" + apellidos1
+				+ ", apellidos2=" + apellidos2 + ", nifCif=" + nifCif + ", movil=" + movil + ", correoElectronico="
+				+ correoElectronico + ", domicilio=" + domicilio + "]";
 	}
-
+	
+	
 }
