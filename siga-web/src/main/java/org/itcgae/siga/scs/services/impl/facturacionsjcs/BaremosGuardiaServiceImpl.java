a1901c1cbc291002093bd5caa348763438db99f2
ES\mmorenomartin <mmorenomartin@deloitte.es>
2021-11-22 09:03:30 +0100

    Baremos guardia: seteo de objeto error en DTO de respuesta.

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/BaremosGuardiaServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/BaremosGuardiaServiceImpl.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.itcgae.siga.DTO.scs.BaremosGuardiaDTO;
 import org.itcgae.siga.DTO.scs.BaremosGuardiaItem;
+import org.itcgae.siga.DTOs.gen.Error;
 import org.itcgae.siga.db.entities.ScsBaremosGuardiaKey;
 import org.itcgae.siga.db.services.scs.mappers.ScsBaremosGuardiaMapper;
 import org.itcgae.siga.scs.services.facturacionsjcs.IBaremosGuardiaServices;
@@ -26,6 +27,7 @@ public class BaremosGuardiaServiceImpl implements IBaremosGuardiaServices {
     public BaremosGuardiaDTO searchBaremosGuardia(Integer idTurno, Integer idGuardia,
                                                       HttpServletRequest request) {
     	BaremosGuardiaDTO baremosGuardiaDTO = new BaremosGuardiaDTO();
+    	Error error = new Error();
 
         LOGGER.info("searchBaremosGuardia() -> Entrada del servicio para obtener baremos guardia ");
 
@@ -33,19 +35,25 @@ public class BaremosGuardiaServiceImpl implements IBaremosGuardiaServices {
 //        String dni = UserTokenUtils.getDniFromJWTToken(token);
         Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
        
+		try {
+			if (null != idInstitucion) {
+				ScsBaremosGuardiaKey key = new ScsBaremosGuardiaKey();
+				key.setIdInstitucion(idInstitucion);
+				key.setIdGuardia(idGuardia);
+				key.setIdTurno(idTurno);
 
-        if (null != idInstitucion) {
-        	ScsBaremosGuardiaKey key = new ScsBaremosGuardiaKey();
-        	key.setIdInstitucion(idInstitucion);
-        	key.setIdGuardia(idGuardia);
-        	key.setIdTurno(idTurno);
-        	
-        	List<BaremosGuardiaItem> lBaremos = baremosGuardiaMapper.searchBaremosGuardia(key);
-        	baremosGuardiaDTO.setBaremosGuardiaItems(lBaremos);
-        	
-        } else {
-            LOGGER.warn("searchBaremosGuardia() -> idInstitucion del token nula");
-        }
+				List<BaremosGuardiaItem> lBaremos = baremosGuardiaMapper.searchBaremosGuardia(key);
+				baremosGuardiaDTO.setBaremosGuardiaItems(lBaremos);
+
+			} else {
+				LOGGER.warn("searchBaremosGuardia() -> idInstitucion del token nula");
+			}
+		} catch (Exception e) {
+			error.setCode(500);
+			error.setDescription("general.mensaje.error.bbdd");
+		}
+        
+        baremosGuardiaDTO.setError(error);
 
         LOGGER.info("searchBaremosGuardia() -> Salida del servicio para obtener baremos guardia");
 
