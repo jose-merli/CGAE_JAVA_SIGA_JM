64bbdc8d57403a37927982e451cc65d0a0fc3eda
José Luis Romero <jromeroprieto@deloitte.es>
2021-11-24 17:24:23 +0100

    cambios en servicio de ficheros de transferencias

--- siga-core/src/main/java/org/itcgae/siga/db/services/fac/mappers/FacDisqueteabonosExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/fac/mappers/FacDisqueteabonosExtendsMapper.java
@@ -19,16 +19,15 @@ public interface FacDisqueteabonosExtendsMapper extends FacDisqueteabonosMapper 
 	@SelectProvider(type = FacDisqueteabonosExtendsSqlProvider.class, method = "getFicherosTransferencias")
 	@Results({
 		@Result(column = "IDINSTITUCION", property = "idInstitucion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "IDDISQUETEABONO", property = "idDisqueteAbonos", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "IDDISQUETEABONO", property = "idDisqueteAbono", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "NOMBREFICHERO", property = "nombreFichero", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "CUENTA_ENTIDAD", property = "cuentaEntidad", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "cod_banco", property = "bancosCodigo", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "FECHA", property = "fechaCreacion", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "totalimporte", property = "facturacion", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "numfacturas", property = "numRecibos", jdbcType = JdbcType.INTEGER),
-		@Result(column = "FECHAMODIFICACION", property = "fechaUltimaModificacion", jdbcType = JdbcType.VARCHAR),
 		@Result(column = "IDSUFIJO", property = "idSufijo", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "SUFIJO", property = "sufijo", jdbcType = JdbcType.VARCHAR)
+		@Result(column = "SUFIJO", property = "sufijo", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "totalimporte", property = "importeTotal", jdbcType = JdbcType.VARCHAR)
 	})
 	List<FicherosAbonosItem> getFicherosTransferencias(FicherosAbonosItem item, String idInstitucion);
 }
