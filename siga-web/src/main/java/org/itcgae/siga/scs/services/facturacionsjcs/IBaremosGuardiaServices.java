9628f2b4a65fe3d2bcf034b4bda4bd7b4a1b4c44
Fernando del Rosal <fdelrosal@deloitte.es>
2021-11-25 17:20:13 +0100

    [fdelrosal] trabajando en busqueda de baremos de guardias

--- siga-web/src/main/java/org/itcgae/siga/scs/services/facturacionsjcs/IBaremosGuardiaServices.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/facturacionsjcs/IBaremosGuardiaServices.java
@@ -3,9 +3,11 @@ package org.itcgae.siga.scs.services.facturacionsjcs;
 import javax.servlet.http.HttpServletRequest;
 
 import org.itcgae.siga.DTO.scs.BaremosGuardiaDTO;
+import org.itcgae.siga.DTO.scs.BaremosGuardiaItem;
+import org.itcgae.siga.DTO.scs.BaremosRequestDTO;
 
 public interface IBaremosGuardiaServices {
 	
-	public BaremosGuardiaDTO searchBaremosGuardia(Integer idTurno, Integer idGuardia, HttpServletRequest request);
+	public BaremosRequestDTO searchBaremosGuardia(BaremosGuardiaItem baremosGuardiaItem, HttpServletRequest request);
 	
 }
