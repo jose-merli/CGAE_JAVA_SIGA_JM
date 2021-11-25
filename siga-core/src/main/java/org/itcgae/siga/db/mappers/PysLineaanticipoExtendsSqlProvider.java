08f81eacd68072e619284240580db30b8c630e98
Guillermo Rejon <grejon@deloitte.es>
2021-11-25 17:00:13 +0100

    Se introduce un implementacion aprcial de una consulta mejorada para los monederos.

--- siga-core/src/main/java/org/itcgae/siga/db/mappers/PysLineaanticipoExtendsSqlProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/mappers/PysLineaanticipoExtendsSqlProvider.java
@@ -42,14 +42,29 @@ public class PysLineaanticipoExtendsSqlProvider extends PysLineaanticipoSqlProvi
         }
 
         query.GROUP_BY("linea.idLinea", "anti.FECHA", "pers.NIFCIF", "pers.idpersona", "anti.idanticipo", "pers.APELLIDOS1", "pers.APELLIDOS2", "pers.NOMBRE", "anti.descripcion", "anti.importeinicial", "linea.importeanticipado");
-        query.ORDER_BY("anti.FECHA desc")
-        ;
+        query.ORDER_BY("anti.FECHA desc");
         
         String sql = "select a.fecha, a.nifcif, a.idpersona, a.nombre_completo, a.idlinea, a.descripcion, sum(a.importe_inicial) as importe_inicial, sum(a.importe_usado) as importe_usado , sum(a.importe_inicial)- sum(a.importe_usado) as importe_restante from( \r\n"+ query + ") a \r\n"
         		+ "group by fecha, nifcif, idpersona, nombre_completo, idlinea, descripcion";
 
 
         return sql;
+        
+        
+        
+        //REVISAR CONSULTA
+//        select min(anti.fecha) as fecha, pers.nifcif, pers.idpersona, (pers.apellidos1 || ' ' || pers.apellidos2 || ', ' || pers.nombre) as nombre_completo, linea.idlinea, --(select a.descripcion from a where a.roworder = 1),  
+//        anti.descripcion,
+//        sum(anti.importeinicial) as importe_inicial, sum(nvl(linea.importeanticipado, 0)) as importe_usado , sum(anti.importeinicial)- sum(nvl(linea.importeanticipado, 0)) as importe_restante  
+//        FROM pys_anticipoletrado anti
+//        INNER JOIN CEN_PERSONA pers on pers.idpersona = anti.idpersona
+//        LEFT OUTER JOIN pys_lineaanticipo linea on linea.idpersona = pers.idpersona
+//        left outer join pys_anticipoletrado antiFirst on anti.idanticipo = antiFirst.idanticipo and anti.idanticipo = antiFirst.idanticipo
+//        WHERE ((linea.idanticipo = anti.idanticipo or linea.idanticipo is null)) 
+//        AND (anti.idinstitucion = 2005 AND TO_CHAR(anti.FECHA, 'DD/MM/YYYY') >= TO_DATE('26/11/2019','DD/MM/YYYY')  AND  TO_CHAR(anti.FECHA, 'DD/MM/YYYY') <= TO_DATE('26/11/2021','DD/MM/YYYY'))
+//        group by  pers.nifcif, pers.idpersona, linea.idlinea, (pers.apellidos1 || ' ' || pers.apellidos2 || ', ' || pers.nombre)
+//        , anti.descripcion
+//        ORDER BY min(anti.FECHA) desc;
     }
     
     public String selectMaxIdLinea(Short idInstitucion, Long idPersona) {
@@ -91,6 +106,8 @@ public class PysLineaanticipoExtendsSqlProvider extends PysLineaanticipoSqlProvi
         query.WHERE("linea.idinstitucion = " + idInstitucion);
         query.WHERE("linea.idPersona = " + idPersona);
         query.WHERE("linea.idLinea = " + idLinea);
+        
+        query.ORDER_BY("anti.FECHA desc");
     	
     	return query.toString();
     }
