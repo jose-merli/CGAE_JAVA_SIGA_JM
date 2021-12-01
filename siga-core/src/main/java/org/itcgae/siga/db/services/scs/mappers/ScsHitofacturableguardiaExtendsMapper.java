def4085ae6a70d88c6f8d71a1e5673a48a1f4145
Miguel Ángel Daza Laprida <mdaza@deloitte.es>
2021-12-01 08:27:20 +0100

    Se añade la etiqueta @Service

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsHitofacturableguardiaExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsHitofacturableguardiaExtendsMapper.java
@@ -1,7 +1,5 @@
 package org.itcgae.siga.db.services.scs.mappers;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.SelectProvider;
@@ -10,20 +8,24 @@ import org.itcgae.siga.DTOs.gen.ComboItem;
 import org.itcgae.siga.DTOs.scs.GuardiasItem;
 import org.itcgae.siga.db.mappers.ScsHitofacturableguardiaMapper;
 import org.itcgae.siga.db.services.scs.providers.ScsHitofacturableguardiaSqlExtendsProvider;
+import org.springframework.stereotype.Service;
 
-public interface ScsHitofacturableguardiaExtendsMapper extends ScsHitofacturableguardiaMapper{
+import java.util.List;
 
-	@SelectProvider(type = ScsHitofacturableguardiaSqlExtendsProvider.class, method = "getBaremos")
-	@Results({ 
+@Service
+public interface ScsHitofacturableguardiaExtendsMapper extends ScsHitofacturableguardiaMapper {
+
+    @SelectProvider(type = ScsHitofacturableguardiaSqlExtendsProvider.class, method = "getBaremos")
+    @Results({
 //		@Result(column = "DESCRIPCION", property = "label", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "NOMBRE", property = "label", jdbcType = JdbcType.VARCHAR),
-		@Result(column = "PRECIO", property = "value", jdbcType = JdbcType.VARCHAR),
-	})
-	List<ComboItem> getBaremos(String idGuardia, String idLenguaje);
-	
-	@SelectProvider(type = ScsHitofacturableguardiaSqlExtendsProvider.class, method = "getCheckSeparacionGuardias")
-	@Results({ 
-		@Result(column = "DIASAPLICABLES", property = "separarGuardia", jdbcType = JdbcType.VARCHAR),
-	})
-	List<GuardiasItem> getCheckSeparacionGuardias(String idGuardia, String idTurno, String idInstitucion);
+            @Result(column = "NOMBRE", property = "label", jdbcType = JdbcType.VARCHAR),
+            @Result(column = "PRECIO", property = "value", jdbcType = JdbcType.VARCHAR),
+    })
+    List<ComboItem> getBaremos(String idGuardia, String idLenguaje);
+
+    @SelectProvider(type = ScsHitofacturableguardiaSqlExtendsProvider.class, method = "getCheckSeparacionGuardias")
+    @Results({
+            @Result(column = "DIASAPLICABLES", property = "separarGuardia", jdbcType = JdbcType.VARCHAR),
+    })
+    List<GuardiasItem> getCheckSeparacionGuardias(String idGuardia, String idTurno, String idInstitucion);
 }
