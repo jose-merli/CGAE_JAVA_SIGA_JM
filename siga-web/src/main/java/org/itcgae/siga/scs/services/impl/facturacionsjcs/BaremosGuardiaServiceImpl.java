6c0d1b14ecb13e3bbbee30221f609403d3cf5d12
Fernando del Rosal <fdelrosal@deloitte.es>
2021-12-10 08:53:33 +0100

    [fdelrosal] guardar baremo de guardia

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/BaremosGuardiaServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/BaremosGuardiaServiceImpl.java
@@ -1,7 +1,9 @@
 package org.itcgae.siga.scs.services.impl.facturacionsjcs;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -27,6 +29,8 @@ import org.itcgae.siga.db.entities.GenFicheroKey;
 import org.itcgae.siga.db.entities.ScsBaremosGuardiaKey;
 import org.itcgae.siga.db.entities.ScsHitofacturable;
 import org.itcgae.siga.db.entities.ScsHitofacturableguardia;
+import org.itcgae.siga.db.entities.ScsHitofacturableguardiaExample;
+import org.itcgae.siga.db.entities.ScsHitofacturableguardiaKey;
 import org.itcgae.siga.db.services.adm.mappers.AdmUsuariosExtendsMapper;
 import org.itcgae.siga.db.services.scs.mappers.ScsBaremosGuardiaMapper;
 import org.itcgae.siga.db.services.scs.mappers.ScsHitofacturableExtendsMapper;
@@ -182,7 +186,7 @@ public class BaremosGuardiaServiceImpl implements IBaremosGuardiaServices {
 
 	@Override
 	@Transactional
-	public BaremosGuardiaDTO insertBaremo(BaremosGuardiaItem baremosGuardiaItem, HttpServletRequest request) {
+	public BaremosGuardiaDTO saveBaremo(List<BaremosGuardiaItem> baremosGuardiaItem, HttpServletRequest request) {
 		Error error = new Error();
 		BaremosGuardiaDTO baremosGuardiaDTO = new BaremosGuardiaDTO();
 		String token = request.getHeader("Authorization");
@@ -205,49 +209,30 @@ public class BaremosGuardiaServiceImpl implements IBaremosGuardiaServices {
 
 			if (usuarios != null && usuarios.size() > 0) {
 
-				// codigo para insertar el nuevo Baremo
+				ScsHitofacturableguardiaExample hfe = new ScsHitofacturableguardiaExample();
+				hfe.createCriteria().andIdinstitucionEqualTo(idInstitucion)
+						.andIdturnoEqualTo(Integer.parseInt(baremosGuardiaItem.get(0).getIdTurno()))
+						.andIdguardiaEqualTo(Integer.parseInt(baremosGuardiaItem.get(0).getIdGuardia()));
 
-				if (response != 0) {
-					error.setCode(200);
-					baremosGuardiaDTO.setError(error);
-				} else {
-					error.setCode(400);
-					error.setDescription("general.message.error.realiza.accion");
-					baremosGuardiaDTO.setError(error);
+				scsHitofacturableguardiaExtendsMapper.deleteByExample(hfe);
+
+				for (BaremosGuardiaItem baremo : baremosGuardiaItem) {
+
+					ScsHitofacturableguardia insertHito = new ScsHitofacturableguardia();
+
+					insertHito.setIdhito(Long.parseLong(baremo.getIdHito()));
+					insertHito.setIdinstitucion(idInstitucion);
+					insertHito.setIdguardia(Integer.parseInt(baremo.getIdGuardia()));
+					insertHito.setIdturno(Integer.parseInt(baremo.getIdTurno()));
+					insertHito.setPreciohito(new BigDecimal(baremo.getPrecioHito()));
+					insertHito.setAgrupar(baremo.getAgrupar());
+					insertHito.setDiasaplicables(baremo.getDias());
+					insertHito.setFechamodificacion(new Date());
+					insertHito.setUsumodificacion(usuarios.get(0).getIdusuario());
+
+					response = scsHitofacturableguardiaExtendsMapper.insertSelective(insertHito);
+
 				}
-
-			}
-		}
-
-		return baremosGuardiaDTO;
-	}
-
-	@Override
-	@Transactional
-	public BaremosGuardiaDTO updateBaremo(BaremosGuardiaItem baremosGuardiaItem, HttpServletRequest request) {
-		Error error = new Error();
-		BaremosGuardiaDTO baremosGuardiaDTO = new BaremosGuardiaDTO();
-		String token = request.getHeader("Authorization");
-		String dni = UserTokenUtils.getDniFromJWTToken(token);
-		Short idInstitucion = UserTokenUtils.getInstitucionFromJWTToken(token);
-		int response = 0;
-
-		if (idInstitucion != null) {
-
-			AdmUsuariosExample exampleUsuarios = new AdmUsuariosExample();
-			exampleUsuarios.createCriteria().andNifEqualTo(dni).andIdinstitucionEqualTo(Short.valueOf(idInstitucion));
-
-			LOGGER.info(
-					"BaremosGuardiaServiceImpl.insertBaremo() -> Entrada a admUsuariosExtendsMapper para obtener información del usuario logeado");
-
-			List<AdmUsuarios> usuarios = admUsuariosExtendsMapper.selectByExample(exampleUsuarios);
-
-			LOGGER.info(
-					"BaremosGuardiaServiceImpl.insertBaremo() -> Salida de admUsuariosExtendsMapper para obtener información del usuario logeado");
-
-			if (usuarios != null && usuarios.size() > 0) {
-
-					//codigo para insertar el nuevo Baremo
 
 				if (response != 0) {
 					error.setCode(200);
