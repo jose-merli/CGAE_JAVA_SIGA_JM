f0ccd4aa1c0d2a122815ad8168ba24b0809bc14d
fdelrosal <fdelrosal@deloitte.es>
2022-01-17 10:22:34 +0100

    solucion al error de deshacer el cierre de un pago en Facturacion SJCS

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/UtilidadesPagoSJCS.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/facturacionsjcs/UtilidadesPagoSJCS.java
@@ -767,21 +767,31 @@ public class UtilidadesPagoSJCS {
 
         for (Integer idpagoJG : idPagos) {
 
-            LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() -> fcsPagosjgExtendsMapper.getFechaEstadoPago() -> Obtenemos la fecha del estado de pago");
-            Date fechaPago = fcsPagosjgExtendsMapper.getFechaEstadoPago(idInstitucion, idpagoJG);
+            try{
+                LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() -> fcsPagosjgExtendsMapper.getFechaEstadoPago() -> Obtenemos la fecha del estado de pago");
+                Date fechaPago = fcsPagosjgExtendsMapper.getFechaEstadoPago(idInstitucion, idpagoJG);
 
-            LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() -> ejecucionPlsPago.ejecutarPLDeshacerCierre() -> Ejecutamos el procedimiento de base de datos");
-            ejecucionPlsPago.ejecutarPLDeshacerCierre(idInstitucion, fechaPago);
+                LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() -> ejecucionPlsPago.ejecutarPLDeshacerCierre() -> Ejecutamos el procedimiento de base de datos");
+                ejecucionPlsPago.ejecutarPLDeshacerCierre(idInstitucion, fechaPago);
 
-            LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() ->fcsPagosEstadospagosMapper.updateByPrimaryKeySelective() -> Actualizamos el estado de pago a ejecutado");
-            FcsPagosEstadospagos record = new FcsPagosEstadospagos();
-            record.setIdinstitucion(idInstitucion);
-            record.setIdpagosjg(idpagoJG);
-            record.setIdestadopagosjg(Short.valueOf(SigaConstants.ESTADO_PAGO_EJECUTADO));
-            record.setFechaestado(new Date());
-            record.setFechamodificacion(new Date());
-            record.setUsumodificacion(usuario.getIdusuario());
-            fcsPagosEstadospagosMapper.updateByPrimaryKeySelective(record);
+                LOGGER.info("UtilidadesPagoSJCS.deshacerCierre() ->fcsPagosEstadospagosMapper.updateByPrimaryKeySelective() -> Actualizamos el estado de pago a ejecutado");
+                FcsPagosEstadospagos record = new FcsPagosEstadospagos();
+                record.setIdinstitucion(idInstitucion);
+                record.setIdpagosjg(idpagoJG);
+                record.setIdestadopagosjg(Short.valueOf(SigaConstants.ESTADO_PAGO_EJECUTADO));
+                record.setFechaestado(new Date());
+                record.setFechamodificacion(new Date());
+                record.setUsumodificacion(usuario.getIdusuario());
+                FcsPagosEstadospagos pagoEdit = fcsPagosEstadospagosMapper.selectByPrimaryKey(record);
+                if(pagoEdit != null ){
+                    fcsPagosEstadospagosMapper.updateByPrimaryKeySelective(record);
+                }else{
+                    fcsPagosEstadospagosMapper.insertSelective(record);
+                }
+
+            }catch (Exception e){
+                throw new Exception("Error al insertar el nuevo estado de la facturacion", e);
+            }
 
         }
 
