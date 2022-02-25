f3d00e6b8d9e1c386c414b353febca33fdae1ac0
Antonio Suárez Bono <asuarezbono@deloitte.es>
2022-02-25 12:30:08 +0100

    SIGARNV-2649: El tramitador de la tarjeta de Servicio de Tramitación de EJG deja de ser obligatorio

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/ejg/GestionEJGServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/ejg/GestionEJGServiceImpl.java
@@ -585,12 +585,17 @@ public class GestionEJGServiceImpl implements IGestionEJG {
 
                     ejg.setGuardiaturnoIdguardia(Integer.parseInt(datos.getIdGuardia()));
                     ejg.setGuardiaturnoIdturno(Integer.parseInt(datos.getIdTurno()));
-                    ejg.setIdpersona(Long.parseLong(datos.getIdPersona()));
+
+                    // Persona de la tarjeta de Servicio de Tramitación (Opcional)
+                    if (!UtilidadesString.esCadenaVacia(datos.getIdPersona()))
+                        ejg.setIdpersona(Long.parseLong(datos.getIdPersona()));
+                    else
+                        ejg.setIdpersona(null);
 
                     LOGGER.info(
                             "guardarServiciosTramitacion() / scsEjgMapper.updateByPrimaryKeySelective() -> Entrada a scsEjgMapper para actualizar el ejg");
 
-                    response = scsEjgMapper.updateByPrimaryKeySelective(ejg);
+                    response = scsEjgMapper.updateByPrimaryKey(ejg);
 
                     LOGGER.info(
                             "guardarServiciosTramitacion() / scsEjgMapper.updateByPrimaryKeySelective() -> Salida a scsEjgMapper para actualizar el ejg");
