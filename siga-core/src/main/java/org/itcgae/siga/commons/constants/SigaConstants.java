7ff770ef50111b44bc33aecf5fe522aaa7906afe
Paco Mansilla <fmansilla@deloitte.es>
2022-08-07 21:32:39 +0200

    Cambio a versión 1.0.82_4

--- siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
+++ siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
@@ -1489,7 +1489,7 @@ public class SigaConstants {
 	public static String EEJG_IDSISTEMA ="EEJG_IDSISTEMA";
 	public static String EEJG_URLWS ="EEJG_URLWS";
 	public static String SIGAFRONT_VERSION = "202109071016";
-	public static String SIGAWEB_VERSION = "1.0.82_3";
+	public static String SIGAWEB_VERSION = "1.0.82_4";
 	
 	public static final String ACREDITACION_TIPO_INICIO = "1";
 	public static final String ACREDITACION_TIPO_FIN = "2";
