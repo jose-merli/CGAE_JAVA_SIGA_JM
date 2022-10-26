fea2d14f9dfcd797b9ff4fd6980f974d1cb9cc21
kvargasnunez <kvargasnunez@deloitte.es>
2022-10-26 17:35:08 +0200

    Modificaciones Pantalla Compra de productos

--- siga-web/src/main/java/org/itcgae/siga/fac/services/impl/FacturacionRapidaServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/fac/services/impl/FacturacionRapidaServiceImpl.java
@@ -663,10 +663,11 @@ public class FacturacionRapidaServiceImpl implements IFacturacionRapidaService {
                         double importeAnticipadoTarjeta = UtilidadesNumeros.redondea(productoBean.getCantidad().doubleValue() * productoBean.getValor().doubleValue() * (1 + (productoBean.getIdtipoiva().floatValue() / 100)), 2);
                         compraBean.setImporteanticipado(BigDecimal.valueOf(new Double(importeAnticipadoTarjeta)));
                     } else {
-                        if (productoBean.getIdformapago().intValue() == Integer.parseInt(SigaConstants.TIPO_FORMAPAGO_METALICO))
-                            compraBean.setImporteanticipado(importeAnticipado);
-                        else
-                            compraBean.setImporteanticipado(BigDecimal.valueOf(new Double(0)));
+                    	try {
+                             compraBean.setImporteanticipado(importeAnticipado);
+                    	}catch(Exception e) {
+                    		compraBean.setImporteanticipado(BigDecimal.valueOf(new Double(0)));
+                    	}     
                     }
                 }
 
