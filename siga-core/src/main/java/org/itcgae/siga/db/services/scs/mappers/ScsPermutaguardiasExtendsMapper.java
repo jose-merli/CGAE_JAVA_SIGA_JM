3b8892147c501e3a8ce7f82e81a4fe13359f9a85
Jesús Rivero <jriveromuniz@deloitte.es>
2022-12-16 08:57:53 +0100

    SIGARNV-2885 - Añadido mapper de la nueva consulta para obtener las permutas de las guardias de colegiado

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsPermutaguardiasExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsPermutaguardiasExtendsMapper.java
@@ -52,6 +52,38 @@ public interface ScsPermutaguardiasExtendsMapper extends ScsPermutaguardiasMappe
 	})
 	List<PermutaItem> getPermutaColeg(PermutaItem permutaItem, Short idInstitucion);
 	
+	@SelectProvider(type = ScsPermutaguardiasSqlExtendsProvider.class, method = "getPermutasGuardiaColeg")
+	@Results({ 
+		@Result(column = "FECHACONFIRMACION", property = "fechaconfirmacion", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "FECHASOLICITUD", property = "fechasolicitud", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "TURNOPERMUTA", property = "nombreTurno", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "GUARDIADESTINO", property = "nombreGuardia", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "MOTIVOS", property = "motivos", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "IDINSTITUCION", property = "idinstitucion", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "numero", property = "numero", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idpersona_solicitante", property = "idpersonaSolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idpersona_confirmador", property = "idpersonaConfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idturno_solicitante", property = "idturnoSolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idcalendarioguardias_solicitan", property = "idcalendarioguardiasSolicitan", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idguardia_solicitante", property = "idguardiaSolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idturno_confirmador", property = "idturnoConfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idcalendarioguardias_confirmad", property = "idcalendarioguardiasConfirmad", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idguardia_confirmador", property = "idguardiaConfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "fechainicio_solicitante", property = "fechainicioSolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "fechainicio_confirmador", property = "fechainicioConfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "motivosconfirmador", property = "motivosconfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "motivossolicitante", property = "motivossolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "id_per_cab_solicitante", property = "idPerCabSolicitante", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "id_per_cab_confirmador", property = "idPerCabConfirmador", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idcalendarioguardias", property = "idcalendarioguardias", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "fecha", property = "fecha", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idturno", property = "idturno", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idguardia", property = "idguardia", jdbcType = JdbcType.VARCHAR),
+		@Result(column = "idpersona", property = "idpersona", jdbcType = JdbcType.VARCHAR),
+
+	})
+	List<PermutaItem> getPermutasGuardiaColeg(PermutaItem permutaItem, Short idInstitucion);
+	
 	@SelectProvider(type = ScsPermutaguardiasSqlExtendsProvider.class, method = "getTurnoInscrito")
 	@Results({ @Result(column = "IDTURNO", property = "value", jdbcType = JdbcType.VARCHAR),
 			@Result(column = "NOMBRE", property = "label", jdbcType = JdbcType.VARCHAR),
