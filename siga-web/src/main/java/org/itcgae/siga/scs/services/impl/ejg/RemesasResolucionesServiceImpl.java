e05490e247cc699d6a4eada82b4cdf88432c691f
Jaime Garcia Carmona <jgarciacarmona@deloitte.es>
2023-04-28 13:04:41 +0200

    SIGARNV-3033 - Soluciono el problema al descargar las resoluciones de remesas.

--- siga-web/src/main/java/org/itcgae/siga/scs/services/impl/ejg/RemesasResolucionesServiceImpl.java
+++ siga-web/src/main/java/org/itcgae/siga/scs/services/impl/ejg/RemesasResolucionesServiceImpl.java
@@ -266,17 +266,14 @@ public class RemesasResolucionesServiceImpl implements IRemesasResoluciones{
               
                   if (remesasResolucionItem.getNombreFichero() != null) {
                 		
-                    String nombreFichero =  remesasResolucionItem.getNombreFichero().substring(0, remesasResolucionItem.getNombreFichero().lastIndexOf("."));
-	                String extension = ".zip";
-	                
-	                String nextEntry = nombreFichero + extension;                
+                    String nombreFichero =  remesasResolucionItem.getNombreFichero();
                     String path = getDirectorioFicheroRemesa(idInstitucion,remesasResolucionItem.getIdRemesaResolucion());
-                    path += File.separator + nombreFichero + extension;      
-                    LOGGER.info("Buscando Fichero = " +nombreFichero + extension);
+                    path += File.separator + nombreFichero;      
+                    LOGGER.info("Buscando Fichero = " +nombreFichero);
                     LOGGER.info("Ruta Completa de Fichero = " + path);
                     File file = new File(path);
                     if(file.exists() && !file.isDirectory()) {
-                  	  zipOutputStream.putNextEntry(new ZipEntry(nextEntry));
+                  	  zipOutputStream.putNextEntry(new ZipEntry(nombreFichero));
   	                  FileInputStream fileInputStream = new FileInputStream(file);
   	                  IOUtils.copy(fileInputStream, zipOutputStream);
   	                  fileInputStream.close();   
@@ -285,10 +282,10 @@ public class RemesasResolucionesServiceImpl implements IRemesasResoluciones{
                     if(remesasResolucionItem.getLog() == 1) {
                   	  path = getDirectorioFicheroRemesa(idInstitucion,remesasResolucionItem.getIdRemesaResolucion());
                   	  path += File.separator + "log" + File.separator +nombreFichero + "_errores.txt";
-                  	  nextEntry = nombreFichero + "_errores.txt";
+                  	nombreFichero = nombreFichero + "_errores.txt";
                   	  File fileLog = new File(path);
                         if(fileLog.exists() && !fileLog.isDirectory()) {
-                      	  zipOutputStream.putNextEntry(new ZipEntry(nextEntry));
+                      	  zipOutputStream.putNextEntry(new ZipEntry(nombreFichero));
       	                  FileInputStream fileInputStream = new FileInputStream(fileLog);
       	                  IOUtils.copy(fileInputStream, zipOutputStream);
       	                  fileInputStream.close();   
