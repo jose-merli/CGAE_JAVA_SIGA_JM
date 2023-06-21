df70274e8021bb41bd334cd769314cb69ada223a
kvargasnunez <kvargasnunez@deloitte.es>
2023-06-21 17:03:08 +0200

    SIGARNV- 1603 : Modificado clase PlantillaItem para tener el compare.

--- siga-core/src/main/java/org/itcgae/siga/DTOs/com/DocumentoPlantillaItem.java
+++ siga-core/src/main/java/org/itcgae/siga/DTOs/com/DocumentoPlantillaItem.java
@@ -68,6 +68,79 @@ public class DocumentoPlantillaItem {
 	public void setIdClaseComunicacion(String idClaseComunicacion) {
 		this.idClaseComunicacion = idClaseComunicacion;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((error == null) ? 0 : error.hashCode());
+		result = prime * result + ((idClaseComunicacion == null) ? 0 : idClaseComunicacion.hashCode());
+		result = prime * result + ((idIdioma == null) ? 0 : idIdioma.hashCode());
+		result = prime * result + ((idInforme == null) ? 0 : idInforme.hashCode());
+		result = prime * result + ((idModeloComunicacion == null) ? 0 : idModeloComunicacion.hashCode());
+		result = prime * result + ((idPlantillaDocumento == null) ? 0 : idPlantillaDocumento.hashCode());
+		result = prime * result + ((idioma == null) ? 0 : idioma.hashCode());
+		result = prime * result + ((nombreDocumento == null) ? 0 : nombreDocumento.hashCode());
+		result = prime * result + ((rutaDocumento == null) ? 0 : rutaDocumento.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DocumentoPlantillaItem other = (DocumentoPlantillaItem) obj;
+		if (error == null) {
+			if (other.error != null)
+				return false;
+		} else if (!error.equals(other.error))
+			return false;
+		if (idClaseComunicacion == null) {
+			if (other.idClaseComunicacion != null)
+				return false;
+		} else if (!idClaseComunicacion.equals(other.idClaseComunicacion))
+			return false;
+		if (idIdioma == null) {
+			if (other.idIdioma != null)
+				return false;
+		} else if (!idIdioma.equals(other.idIdioma))
+			return false;
+		if (idInforme == null) {
+			if (other.idInforme != null)
+				return false;
+		} else if (!idInforme.equals(other.idInforme))
+			return false;
+		if (idModeloComunicacion == null) {
+			if (other.idModeloComunicacion != null)
+				return false;
+		} else if (!idModeloComunicacion.equals(other.idModeloComunicacion))
+			return false;
+		if (idPlantillaDocumento == null) {
+			if (other.idPlantillaDocumento != null)
+				return false;
+		} else if (!idPlantillaDocumento.equals(other.idPlantillaDocumento))
+			return false;
+		if (idioma == null) {
+			if (other.idioma != null)
+				return false;
+		} else if (!idioma.equals(other.idioma))
+			return false;
+		if (nombreDocumento == null) {
+			if (other.nombreDocumento != null)
+				return false;
+		} else if (!nombreDocumento.equals(other.nombreDocumento))
+			return false;
+		if (rutaDocumento == null) {
+			if (other.rutaDocumento != null)
+				return false;
+		} else if (!rutaDocumento.equals(other.rutaDocumento))
+			return false;
+		return true;
+	}
+	
+	
 	
 
 }
