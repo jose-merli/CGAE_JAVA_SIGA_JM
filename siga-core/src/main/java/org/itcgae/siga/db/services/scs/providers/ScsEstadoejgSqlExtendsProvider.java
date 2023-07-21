3d9138e7d430152423ba04750b03936cdacce309
Francisco Mansilla de Sebastián <fmansilla@deloitte.es>
2023-07-21 15:27:01 +0200

    Revert "Revert "SIGARNV-3493 - Se aplican filtros en el combo estados de EJG para que se muestren distintas opciones según se venga de remesas de envío o de EJG""
    This reverts commit bb0984d187cc21412a0c7033f67ac864af3d4969.

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsEstadoejgSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsEstadoejgSqlExtendsProvider.java
@@ -39,12 +39,29 @@ public class ScsEstadoejgSqlExtendsProvider extends ScsEstadoejgSqlProvider {
                     return sql.toString();
           }
 
-	public String comboEstadoEjg(Short idLenguaje) {
+	public String comboEstadoEjg(Short idLenguaje, String filtroEstadoEjg) {
                     SQL sql = new SQL();
 
         sql.SELECT("IDESTADOEJG, F_SIGA_GETRECURSO(DESCRIPCION, 1) AS DESCRIPCION, CODIGOEXT, BLOQUEADO, ORDEN, VISIBLECOMISION, EDITABLECOMISION");
         sql.FROM("SCS_MAESTROESTADOSEJG");
-        sql.WHERE("IDESTADOEJG NOT IN (25,26,13,0,9,10,12,15,16,20)");
+        switch(filtroEstadoEjg) {
+        
+        	case "1" :
+        		sql.WHERE("IDESTADOEJG NOT IN (25,26,13,0,9,10,12,15,16,20)");
+        		
+        		break;
+        		
+        	case "2" :
+        			
+        		break;
+        		
+        	default:
+        		sql.WHERE("IDESTADOEJG NOT IN (25,26,13,0,9,10,12,15,16,20)");
+        		
+        		break;
+        	
+        }
+       
         sql.ORDER_BY("ORDEN");
 
         return sql.toString();
