051c02de4bfa1f6595b316c21a2bf7151291c9fe
Francisco Mansilla de Sebastián <fmansilla@deloitte.es>
2023-11-17 09:04:30 +0100

    Actualizo versión 1.0.108_2

--- siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
+++ siga-core/src/main/java/org/itcgae/siga/commons/constants/SigaConstants.java
@@ -1568,7 +1568,7 @@ public class SigaConstants {
 	public static String EEJG_IDSISTEMA ="EEJG_IDSISTEMA";
 	public static String EEJG_URLWS ="EEJG_URLWS";
 	public static String SIGAFRONT_VERSION = "202109071016";
-	public static String SIGAWEB_VERSION = "1.0.108_1";
+	public static String SIGAWEB_VERSION = "1.0.108_2";
 	
 	public static final String ACREDITACION_TIPO_INICIO = "1";
 	public static final String ACREDITACION_TIPO_FIN = "2";
