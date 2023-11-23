06cabc364ecf1d6d2774ad158199ab1aa062df2e
Jesús Rivero <jriveromuniz@deloitte.es>
2023-11-23 10:23:20 +0100

    SIGARNV-3804 - Modificada descarga de documentación al realizar comunicaciones para poder descargar sin destinatarios

--- siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ModModeloPlantillaDocumentoExtendsSqlProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/providers/ModModeloPlantillaDocumentoExtendsSqlProvider.java
@@ -71,8 +71,9 @@ public class ModModeloPlantillaDocumentoExtendsSqlProvider {
 		sql.FROM("mod_modelo_plantilladocumento modPlantilla");	
 		sql.INNER_JOIN("mod_plantilladocumento ON mod_plantilladocumento.idplantilladocumento = modPlantilla.idplantilladocumento AND mod_plantilladocumento.idioma = '"+idLenguaje+"'");
 		sql.INNER_JOIN(" MOD_PLANTILLADOC_CONSULTA ON  MOD_PLANTILLADOC_CONSULTA.IDPLANTILLADOCUMENTO = modPlantilla.IDPLANTILLADOCUMENTO");
-		sql.INNER_JOIN("con_consulta ON con_consulta.idconsulta=MOD_PLANTILLADOC_CONSULTA.Idconsulta AND con_consulta.idinstitucion = MOD_PLANTILLADOC_CONSULTA.Idinstitucion_consulta and con_consulta.fechabaja is null");
-		sql.WHERE("modPlantilla.IDMODELOCOMUNICACION = " + idModeloComunicacion + " AND modPlantilla.FECHABAJA IS NULL and con_consulta.idobjetivo = 1");
+		sql.INNER_JOIN("con_consulta ON con_consulta.idconsulta=MOD_PLANTILLADOC_CONSULTA.Idconsulta AND con_consulta.idinstitucion = MOD_PLANTILLADOC_CONSULTA.Idinstitucion_consulta");
+		sql.WHERE("modPlantilla.IDMODELOCOMUNICACION = " + idModeloComunicacion + " AND modPlantilla.FECHABAJA IS NULL and con_consulta.idobjetivo = 1 AND mod_plantilladoc_consulta.fechabaja IS NULL"
+				+ "      AND con_consulta.fechabaja IS NULL");
 		sql.GROUP_BY("modPlantilla.IDINFORME,modPlantilla.NOMBREFICHEROSALIDA, modPlantilla.FORMATOSALIDA, modPlantilla.IDPLANTILLADOCUMENTO");
 		
 		return sql.toString();
