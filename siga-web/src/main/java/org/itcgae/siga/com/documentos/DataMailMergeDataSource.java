d21e7e3fe5bad2b058ddfd8fa8eba0de90b33c6e
Jesús Rivero <jriveromuniz@deloitte.es>
2024-01-11 10:10:55 +0100

    Revert "SIGARNV-3276"
    This reverts commit 639d6e97e64395f3b20e0dfc717c49b637296bc4.

--- siga-web/src/main/java/org/itcgae/siga/com/documentos/DataMailMergeDataSource.java
+++ siga-web/src/main/java/org/itcgae/siga/com/documentos/DataMailMergeDataSource.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.aspose.words.IMailMergeDataSource;
-import com.aspose.words.ref.Ref;
 
 
 /**
@@ -80,13 +79,6 @@ public class DataMailMergeDataSource implements IMailMergeDataSource{
 			throws Exception {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-
-	@Override
-	public boolean getValue(String arg0, Ref<Object> arg1) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
