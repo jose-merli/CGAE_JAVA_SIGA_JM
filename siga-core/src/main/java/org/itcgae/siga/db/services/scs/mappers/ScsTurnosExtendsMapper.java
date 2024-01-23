836af5e5d380a3644c84d21d7c8d1630aa32e330
María Pozo Estrada <mpozoestrada@deloitte.es>
2024-01-23 15:09:07 +0100

    SIGARNV-3912 - Al seleccionar un turno se rellenan los datos del colegiado con los datos del siguiente colegiado de la cola del turno seleccionado

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsTurnosExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/mappers/ScsTurnosExtendsMapper.java
@@ -259,6 +259,40 @@ public interface ScsTurnosExtendsMapper extends ScsTurnoMapper {
 			@Result(column = "ORDEN_COLA", property = "idcomboguardias", jdbcType = JdbcType.DECIMAL) })
 	List<TurnosItem> busquedaColaOficio2(TurnosItem turnosItem,String busquedaOrden,String strDate,Short idInstitucion);
 
+	@SelectProvider(type=ScsTurnosSqlExtendsProvider.class, method="busquedaColaOficioPrimerLetrado")
+	@Results({ @Result(column = "IDINSTITUCION", property = "idinstitucion", jdbcType = JdbcType.DECIMAL, id = true),
+			@Result(column = "IDTURNO", property = "idturno", jdbcType = JdbcType.VARCHAR, id = true),
+			@Result(column = "NOMBRE", property = "nombre", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ABREVIATURA", property = "abreviatura", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "GUARDIAS", property = "guardias", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "VALIDARJUSTIFICACIONES", property = "validarjustificaciones", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "DESIGNADIRECTA", property = "designadirecta", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "VALIDARINSCRIPCIONES", property = "validarinscripciones", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDAREA", property = "idarea", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDMATERIA", property = "idmateria", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDZONA", property = "idzona", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDORDENACIONCOLAS", property = "idordenacioncolas", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDGRUPOFACTURACION", property = "idgrupofacturacion", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "FECHAMODIFICACION", property = "fechamodificacion", jdbcType = JdbcType.TIMESTAMP),
+			@Result(column = "USUMODIFICACION", property = "usumodificacion", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDSUBZONA", property = "idsubzona", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDPARTIDAPRESUPUESTARIA", property = "idpartidapresupuestaria", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "REQUISITOS", property = "requisitos", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDPERSONA_ULTIMO", property = "idpersona_ultimo", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "DESCRIPCION", property = "descripcion", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "ACTIVARRETRICCIONACREDIT", property = "activarretriccionacredit", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "LETRADOASISTENCIAS", property = "letradoasistencias", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "LETRADOACTUACIONES", property = "letradoactuaciones", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "CODIGOEXT", property = "codigoext", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "FECHASOLICITUD_ULTIMO", property = "fechasolicitudUltimo", jdbcType = JdbcType.TIMESTAMP),
+			@Result(column = "VISIBILIDAD", property = "visibilidad", jdbcType = JdbcType.VARCHAR),
+			@Result(column = "IDTIPOTURNO", property = "idtipoturno", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "VISIBLEMOVIL", property = "visiblemovil", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "IDJURISDICCION", property = "idjurisdiccion", jdbcType = JdbcType.DECIMAL),
+			@Result(column = "FECHABAJA", property = "fechabaja", jdbcType = JdbcType.TIMESTAMP),
+			@Result(column = "ORDEN_COLA", property = "idcomboguardias", jdbcType = JdbcType.DECIMAL) })
+	List<TurnosItem> busquedaColaOficioPrimerLetrado(TurnosItem turnosItem,String busquedaOrden,String strDate,Short idInstitucion);
+	
 	@SelectProvider(type=ScsTurnosSqlExtendsProvider.class, method="busquedaColaGuardia")
 	@Results({
 			@Result(column = "ORDEN_COLA", property = "orden", jdbcType = JdbcType.VARCHAR),
