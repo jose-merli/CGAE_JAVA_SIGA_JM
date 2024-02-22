e48f58a190bd267101b26ee4704284340e88638b
Francisco Mansilla de Sebastián <fmansilla@deloitte.es>
2024-02-22 16:28:43 +0100

    Revert "SIGAARMV-3896 info de idiomas plantillas en consultas asociadas"
    This reverts commit 0cadf01888ccd87dea7c46fc9a5e40e011a936e4.

--- siga-core/src/main/java/org/itcgae/siga/db/services/com/mappers/ModPlantillaDocumentoConsultaExtendsMapper.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/com/mappers/ModPlantillaDocumentoConsultaExtendsMapper.java
@@ -80,7 +80,4 @@ public interface ModPlantillaDocumentoConsultaExtendsMapper {
 	
 	@SelectProvider(type = ModPlantillaDocumentoConsultaExtendsSqlProvider.class, method = "selectConsultasDestinatarioByModelo")
 	List<ConsultaItem> selectConsultasDestinatarioByModelo(Short idInstitucion, Long idModeloComunicacion, Long idObjetivo, String idioma,String idModeloPlantilla);
-	
-	@SelectProvider(type = ModPlantillaDocumentoConsultaExtendsSqlProvider.class, method = "selectIdiomasPlantillasConsultas")
-	List<String> selectIdiomasPlantillasConsultas(Short idInstitucion, Long idModeloComunicacion, Long idInforme, Long idConsulta);
 }
