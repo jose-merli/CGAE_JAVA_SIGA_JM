e6f4ccf80edba2638b2a22a4b2140de3567362a9
Antonio Rincón Ramos <anrinconramos@deloitte.es>
2024-06-05 15:46:09 +0200

    SIGARNV-4555: se ha modificado la consulta

--- siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsDesignacionesSqlExtendsProvider.java
+++ siga-core/src/main/java/org/itcgae/siga/db/services/scs/providers/ScsDesignacionesSqlExtendsProvider.java
@@ -2363,7 +2363,7 @@ public class ScsDesignacionesSqlExtendsProvider extends ScsDesignaSqlProvider {
 		SQL sql = new SQL();
 
 		sql.SELECT("DESIGNA.IDINSTITUCION");
-		sql.SELECT("concat('D' || DESIGNA.anio || '/', CASE WHEN LENGTH(TO_NUMBER(DESIGNA.codigo)) < 5 THEN LPAD(TO_NUMBER(DESIGNA.codigo),5,'0') ELSE TO_CHAR(TO_NUMBER(DESIGNA.codigo)) END ) as asunto");
+		sql.SELECT("('D' || DESIGNA.anio || '/' || DESIGNA.codigo) as asunto");
 		sql.SELECT("DESIGNA.FECHAENTRADA as fecha");
 		sql.SELECT("DESIGNA.ANIO");
 		sql.SELECT("DESIGNA.NUMERO");
